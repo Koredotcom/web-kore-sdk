77f3caa0f015ea0fcd8bed2f28bb1cf55058190b
sainathbhima <you@example.com>
2018-12-21 12:12:15 +0530

    seconds resolution in SDK chat history

--- UI/chatWindow.js
+++ UI/chatWindow.js
@@ -158,11 +158,12 @@ function koreBotChat() {
         'formatAMPM': function (date) {
             var hours = date.getHours();
             var minutes = date.getMinutes();
+            var seconds = date.getSeconds();
             var ampm = hours >= 12 ? 'pm' : 'am';
             hours = hours % 12;
             hours = hours ? hours : 12; // the hour '0' should be '12'
             minutes = minutes < 10 ? '0' + minutes : minutes;
-            var strTime = hours + ':' + minutes + ' ' + ampm;
+            var strTime = hours + ':' + minutes + ':'+ seconds + ' ' + ampm;
             return strTime;
         },
         'formatDate': function (date) {
