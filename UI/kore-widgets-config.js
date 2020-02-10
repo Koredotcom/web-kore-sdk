c6c80c1100b04c2f78019aa974d6a7e3266b7337
rajasekhar <rajasekhar.balla@kore.com>
2020-02-10 20:45:23 +0530

    corrected API url

--- UI/kore-widgets-config.js
+++ UI/kore-widgets-config.js
@@ -4,7 +4,7 @@
 
     var botOptionsWiz = {};
     botOptionsWiz.logLevel = 'debug';
-    botOptionsWiz.koreAPIUrl = "https://bots.kore.ai/api/";
+    botOptionsWiz.koreAPIUrl = "https://bots.kore.ai";
 
     botOptionsWiz.JWTUrl = "PLEASE_ENTER_JWTURL_HERE";
     botOptionsWiz.userIdentity = 'PLEASE_ENTER_USER_EMAIL_ID';// Provide users email id here
