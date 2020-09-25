602af9399907f8b7ab69509a8d6774f1e923b719
Ajay <ajaybabuputti@gmail.com>
2020-09-26 02:04:22 +0530

    Update chatWindow.js
    Seconds to be displayed with 0 infront if it is a single digit.

--- UI/chatWindow.js
+++ UI/chatWindow.js
@@ -183,7 +183,7 @@
                 'formatAMPM': function (date) {
                     var hours = date.getHours();
                     var minutes = date.getMinutes();
-                    var seconds = date.getSeconds();
+                    var seconds = ('0'+date.getSeconds()).slice(-2);
                     var ampm = hours >= 12 ? 'pm' : 'am';
                     hours = hours % 12;
                     hours = hours ? hours : 12; // the hour '0' should be '12'
