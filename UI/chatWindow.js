609aec69a5ba5bbe77e76f93126288b23bd78c6c
RajasekharBa-Kore <rajasekhar.balla@kore.com>
2020-11-11 18:36:02 +0530

    polyfill added for String.includes method

--- UI/chatWindow.js
+++ UI/chatWindow.js
@@ -159,6 +159,18 @@
                 var target = this;
                 return target.replace(new RegExp(search, 'g'), replacement);
             };
+            
+            if (!String.prototype.includes) {
+              String.prototype.includes = function(search, start) {
+                'use strict';
+
+                if (search instanceof RegExp) {
+                  throw TypeError('first argument must not be a RegExp');
+                } 
+                if (start === undefined) { start = 0; }
+                return this.indexOf(search, start) !== -1;
+              };
+            }
 
             function findSortedIndex(array, value) {
                 var low = 0,
