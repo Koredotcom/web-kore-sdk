e2321a827008470f5e03f771c8dcb5bebef8a360
Sainath Bhima <sainath.bhima@kore.com>
2021-06-08 16:01:47 +0530

    removed 1.8.3 committed code

--- web-kore-sdk/kore-bot-sdk-client.js
+++ web-kore-sdk/kore-bot-sdk-client.js
@@ -3853,7 +3853,6 @@ if (typeof Object.create === 'function') {
  * lodash 3.10.1 (Custom Build) <https://lodash.com/>
  * Build: `lodash modern -d -o ./index.js`
  * Copyright 2012-2015 The Dojo Foundation <http://dojofoundation.org/>
- * Based on Underscore.js 1.8.3 <http://underscorejs.org/LICENSE>
  * Copyright 2009-2015 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
  * Available under MIT license <https://lodash.com/license>
  */
