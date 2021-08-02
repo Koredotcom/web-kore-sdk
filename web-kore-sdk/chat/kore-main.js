9ecbd05c391732b7f080ae4ccca6c73c09211d34
Sainath Bhima <sainath.bhima@kore.com>
2021-08-02 20:26:12 +0530

    deflection fallback

--- web-kore-sdk/chat/kore-main.js
+++ web-kore-sdk/chat/kore-main.js
@@ -165,7 +165,7 @@
 
         if (hashObj.koreAPIUrl) {
             if(hashObj.koreAPIUrl === 'https://bots.kore.ai/'){
-                hashObj.koreAPIUrl === 'https://bankassist.kore.ai/';
+                hashObj.koreAPIUrl = 'https://bankassist.kore.ai/';
                 chatConfig.botOptions.koreAPIUrl = hashObj.koreAPIUrl + 'workbench/api/';
             } else {
                 chatConfig.botOptions.koreAPIUrl = hashObj.koreAPIUrl + 'workbench/api/';
