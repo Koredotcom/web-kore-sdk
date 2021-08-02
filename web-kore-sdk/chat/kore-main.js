8f04f93609d1e750b4ce7dc9631cf5abf4a7e1e7
Sainath Bhima <sainath.bhima@kore.com>
2021-08-02 18:49:37 +0530

    fallback for bots.kore.ai

--- web-kore-sdk/chat/kore-main.js
+++ web-kore-sdk/chat/kore-main.js
@@ -164,7 +164,12 @@
         }
 
         if (hashObj.koreAPIUrl) {
-            chatConfig.botOptions.koreAPIUrl = hashObj.koreAPIUrl + 'workbench/api/';
+            if(hashObj.koreAPIUrl === 'https://bots.kore.ai'){
+                hashObj.koreAPIUrl === 'https://bankassist.kore.ai';
+                chatConfig.botOptions.koreAPIUrl = hashObj.koreAPIUrl + 'workbench/api/';
+            } else {
+                chatConfig.botOptions.koreAPIUrl = hashObj.koreAPIUrl + 'workbench/api/';
+            }
         }
 
         if (hashObj.brand && hashObj.brand.headerTitle) {
