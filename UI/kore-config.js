1ecd9a7d8603a9b7fdb75080aa543c583ae6eeee
Srinivas rao <srinivasarao.yasarla@kore.com>
2021-09-16 13:28:51 +0530

    Added Note

--- UI/kore-config.js
+++ UI/kore-config.js
@@ -14,6 +14,12 @@
     botOptions.JWTUrl = "PLEASE_ENTER_JWTURL_HERE";
     botOptions.userIdentity = 'PLEASE_ENTER_USER_EMAIL_ID';// Provide users email id here
     botOptions.botInfo = { name: "PLEASE_ENTER_BOT_NAME", "_id": "PLEASE_ENTER_BOT_ID" }; // bot name is case sensitive
+
+    /* 
+    Important Note: These keys are provided here for quick demos to generate JWT token at client side but not for Production environment.
+    Refer below document for JWT token generation at server side. Client Id and Client secret should maintained at server end.
+    https://developer.kore.ai/docs/bots/sdks/user-authorization-and-assertion/
+    **/
     botOptions.clientId = "PLEASE_ENTER_CLIENT_ID";
     botOptions.clientSecret = "PLEASE_ENTER_CLIENT_SECRET";
 	
