3ae46ed7c43fa43470eaa8631fa6bd1d64bad0fb
Rajasekhar Balla <rajasekhar.balla@kore.com>
2021-10-27 18:24:26 +0530

    umd updates

--- src/examples/esm/index_chat.js
+++ src/examples/esm/index_chat.js
@@ -2,46 +2,6 @@
 
 import {chatConfig,chatWindow} from '../../../dist/kore-web-sdk.esm.browser.js';
 import {Korei18nPlugin} from '../../../dist/kore-web-sdk.esm.browser.js';
-var $ =chatWindow.$;
-debugger
-//var chatConfig=KoreSDK2.chatConfig;
-
-// function assertion(options, callback) {
-//     var jsonData = {
-//         "clientId": options.clientId,
-//         "clientSecret": options.clientSecret,
-//         "identity": options.userIdentity,
-//         "aud": "",
-//         "isAnonymous": false
-//     };
-//     $.ajax({
-//         url: options.JWTUrl,
-//         type: 'post',
-//         data: jsonData,
-//         dataType: 'json',
-//         success: function (data) {
-//             options.assertion = data.jwt;
-//             options.handleError = chatWindowInstance.showError;
-//             options.chatHistory = chatWindowInstance.chatHistory;
-//             options.botDetails = chatWindowInstance.botDetails;
-//             callback(null, options);
-//             setTimeout(function () {
-//                 if (chatWindowInstance && chatWindowInstance.initToken) {
-//                     chatWindowInstance.initToken(options);
-//                 }
-//             }, 2000);
-//         },
-//         error: function (err) {
-//             chatWindowInstance.showError(err.responseText);
-//         }
-//     });
-// }
-// var chatConfig=window.KoreSDK.chatConfig;
-// chatConfig.botOptions.assertionFn=assertion;
-
-
-
-
 
 var chatWindowInstance = new chatWindow(chatConfig);
 
--- src/examples/umd/index_chat.js
+++ src/examples/umd/index_chat.js
@@ -2,45 +2,19 @@
 
 var chatConfig=KoreSDK.chatConfig;
 var chatWindow=KoreSDK.chatWindow;
-var $ =chatWindow.$;
+
 debugger
-//var chatConfig=KoreSDK2.chatConfig;
-
-function assertion(options, callback) {
-    var jsonData = {
-        "clientId": options.clientId,
-        "clientSecret": options.clientSecret,
-        "identity": options.userIdentity,
-        "aud": "",
-        "isAnonymous": false
-    };
-    $.ajax({
-        url: options.JWTUrl,
-        type: 'post',
-        data: jsonData,
-        dataType: 'json',
-        success: function (data) {
-            options.assertion = data.jwt;
-            options.handleError = chatWindowInstance.showError;
-            options.chatHistory = chatWindowInstance.chatHistory;
-            options.botDetails = chatWindowInstance.botDetails;
-            callback(null, options);
-            setTimeout(function () {
-                if (chatWindowInstance && chatWindowInstance.initToken) {
-                    chatWindowInstance.initToken(options);
-                }
-            }, 2000);
-        },
-        error: function (err) {
-            chatWindowInstance.showError(err.responseText);
-        }
-    });
-}
-// var chatConfig=window.KoreSDK.chatConfig;
-chatConfig.botOptions.assertionFn=assertion;
-
-
-
 var chatWindowInstance = new chatWindow(chatConfig);
 
-chatWindowInstance.show(chatConfig);
+// chatWindow.prototype.show=function(){
+//     console.log('overridedn')
+// }
+
+//chatWindowInstance.installPlugin(Korei18nPlugin);
+chatWindowInstance.getJWT(chatConfig.botOptions).then(function(res){
+    chatWindowInstance.setJWT(res.jwt);
+    chatWindowInstance.show(chatConfig);
+
+},function(errRes){
+
+});
