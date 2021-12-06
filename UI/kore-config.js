87d3d3f249bb99e04705bbaa45e6379deb52c82c
Rajasekhar Balla <rajasekhar.balla@kore.com>
2021-12-06 18:38:23 +0530

    added hosted sdk support

--- UI/kore-config.js
+++ UI/kore-config.js
@@ -46,12 +46,12 @@
         isSpeechEnabled: false,			// set true, to hide mic icon
         allowGoogleSpeech: true,		// set true, to use Google speech engine instead KORE.AI engine.This feature requires valid Google speech API key. (Place it in 'web-kore-sdk/libs/speech/key.js')
         allowLocation: true,			// set false, to deny sending location to server
-        loadHistory: true,				// set true to load recent chat history
+        loadHistory: false,				// set true to load recent chat history
         messageHistoryLimit: 10,		// set limit to load recent chat history
         autoEnableSpeechAndTTS: false, 	// set true, to use talkType voice keyboard.
         graphLib: "d3" ,				// set google, to render google charts.This feature requires loader.js file which is available in google charts documentation.
         googleMapsAPIKey:"",
-        minimizeMode: true,             // set true, to show chatwindow in minimized mode, If false is set remove #chatContainer style in chatwindow.css  
+        minimizeMode: false,             // set true, to show chatwindow in minimized mode, If false is set remove #chatContainer style in chatwindow.css  
         multiPageApp: {
             enable: false,              //set true for non SPA(Single page applications)
             userIdentityStore: 'localStorage',//'localStorage || sessionStorage'
