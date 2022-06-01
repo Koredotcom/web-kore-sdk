ef3f86590e6698b7c5127379aec47bbf44737f9d
Rajasekhar Balla <rajasekhar.balla@kore.com>
2022-06-01 17:36:24 +0530

    umd fix for emojione

--- examples/umd/chat/js/index_chat.js
+++ examples/umd/chat/js/index_chat.js
@@ -3,10 +3,10 @@
 var chatConfig=KoreChatSDK.chatConfig;
 var chatWindow=KoreChatSDK.chatWindow;
 
-var KorePickersPlugin=KorePickersPluginSDK.KorePickersPlugin;
-var GraphTemplatesPlugin=KoreGraphTemplatesPluginSDK.GraphTemplatesPlugin;
-var SpeechToTextPlugin=SpeechToTextPluginSDK.SpeechToTextPlugin;
-var TtsSpeechPlugin=TtsSpeechPluginSDK.TtsSpeechPlugin;
+// var KorePickersPlugin=KorePickersPluginSDK.KorePickersPlugin;
+// var GraphTemplatesPlugin=KoreGraphTemplatesPluginSDK.GraphTemplatesPlugin;
+// var SpeechToTextPlugin=SpeechToTextPluginSDK.SpeechToTextPlugin;
+// var TtsSpeechPlugin=TtsSpeechPluginSDK.TtsSpeechPlugin;
 
 var chatWindowInstance = new chatWindow();
 
@@ -17,21 +17,32 @@ botOptions.userIdentity = 'PLEASE_ENTER_USER_EMAIL_ID';// Provide users email id
 botOptions.botInfo = { name: "PLEASE_ENTER_BOT_NAME", "_id": "PLEASE_ENTER_BOT_ID" }; // bot name is case sensitive
 botOptions.clientId = "PLEASE_ENTER_CLIENT_ID";
 botOptions.clientSecret = "PLEASE_ENTER_CLIENT_SECRET";
+
+
+
+
+botOptions.JWTUrl = "https://mk2r2rmj21.execute-api.us-east-1.amazonaws.com/dev/users/sts";
+botOptions.userIdentity = 'rajasekhar.balla@kore.com';// Provide users email id here
+botOptions.botInfo = { name: "SDKBot", "_id": "st-b9889c46-218c-58f7-838f-73ae9203488c" }; // bot name is case sensitive
+botOptions.clientId = "cs-1e845b00-81ad-5757-a1e7-d0f6fea227e9";
+botOptions.clientSecret = "5OcBSQtH/k6Q/S6A3bseYfOee02YjjLLTNoT1qZDBso=";
+
+
 // chatWindow.prototype.show=function(){
 //     console.log('overridedn')
 // }
 
-chatWindowInstance.installPlugin(new KorePickersPlugin());
-chatWindowInstance.installPlugin(new GraphTemplatesPlugin());
-chatWindowInstance.installPlugin(new SpeechToTextPlugin());
-chatWindowInstance.installPlugin(new TtsSpeechPlugin());
+// chatWindowInstance.installPlugin(new KorePickersPlugin());
+// chatWindowInstance.installPlugin(new GraphTemplatesPlugin());
+// chatWindowInstance.installPlugin(new SpeechToTextPlugin());
+// chatWindowInstance.installPlugin(new TtsSpeechPlugin());
 
-chatConfig.JWTAsertion=function(commitJWT){
-    chatWindowInstance.getJWT(chatConfig.botOptions).then(function(res){
-        chatWindowInstance.setJWT(res.jwt);
-        commitJWT();
-    },function(errRes){
-        console.log(errRes);
-    });
- };
+// chatConfig.JWTAsertion=function(commitJWT){
+//     chatWindowInstance.getJWT(chatConfig.botOptions).then(function(res){
+//         chatWindowInstance.setJWT(res.jwt);
+//         commitJWT();
+//     },function(errRes){
+//         console.log(errRes);
+//     });
+//  };
  chatWindowInstance.show(chatConfig);
