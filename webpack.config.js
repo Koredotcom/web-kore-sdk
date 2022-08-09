0034de0fd48467f0085a1d6c941cab60f408a3a0
UdayAp-Kore <uday.appam@kore.com>
2022-08-09 16:54:34 +0530

    agent desktop revert

--- webpack.config.js
+++ webpack.config.js
@@ -164,11 +164,11 @@ module.exports= function(env,argv){
             filename: 'plugins/browser-tts-umd-plugin-umd.js',
             chunkLoading: false, // Disable chunks that are loaded on demand and put everything in the main chunk.
           },
-          AgentDeskTopPluginSDK: {
-            import: "./src/index_plugins/agentDesktop_umd.ts",
-            filename: 'plugins/agent-desktop-umd.js',
-            chunkLoading: false, // Disable chunks that are loaded on demand and put everything in the main chunk.
-          },
+          // AgentDeskTopPluginSDK: {
+          //   import: "./src/index_plugins/agentDesktop_umd.ts",
+          //   filename: 'plugins/agent-desktop-umd.js',
+          //   chunkLoading: false, // Disable chunks that are loaded on demand and put everything in the main chunk.
+          // }
         }
         config.output.library = {
           name: '[name]',
