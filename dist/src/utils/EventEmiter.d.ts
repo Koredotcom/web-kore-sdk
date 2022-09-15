daf26db2bd818279225624a63fefecf4b2da1656
KI0546\Rakesh.Manda <rakesh.manda@kore.com>
2022-09-15 16:37:58 +0530

    event emitter callback optional change

--- dist/src/utils/EventEmiter.d.ts
+++ dist/src/utils/EventEmiter.d.ts
@@ -4,8 +4,8 @@ declare class EventEmiter {
     runInAnyEvent: any;
     constructor(parentEmitter: any);
     addNode(event: any, cb: any, once: any): any;
-    on(event: any, cb: any, once: any): () => void;
-    once(event: any, cb: any): () => void;
+    on(event: any, cb?: any, once?: any): () => void;
+    once(event: any, cb?: any): () => void;
     emit(event: string | number, data: any): void;
 }
 export default EventEmiter;
