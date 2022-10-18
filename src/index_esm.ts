94dfcca2b9eebbfe56dd638c8aa278f956ef8970
KI0546\Rakesh.Manda <rakesh.manda@kore.com>
2022-10-18 17:14:51 +0530

    commented speakTextWithAWSPolly plugin

--- src/index_esm.ts
+++ src/index_esm.ts
@@ -7,7 +7,6 @@ import KorePickersPlugin from './plugins/korePickers';
 import GraphTemplatesPlugin from './plugins/graphTemplatesPlugin';
 import WebKitSTT from './plugins/STTPlugins/WebKitSTTPlugin/WebKitSTTPlugin';
 import BrowserTTS from './plugins/TTSPlugins/BrowserTTSPlugin/BrowserTTSPlugin';
-import speakTextWithAWSPolly from './plugins/TTSPlugins/KoreAWSPollyPlugin/kore-aws-polly';
 // import AgentDesktopPlugin from './plugins/agentDesktop/agentdesktop';
 //import speakTextWithAWSPolly from './plugins/TTSPlugins/KoreAWSPollyPlugin/kore-aws-polly';
 import AgentDesktopPlugin from './plugins/agentDesktop/agentdesktop';
@@ -29,7 +28,6 @@ export {
   GraphTemplatesPlugin,
   WebKitSTT,
   BrowserTTS,
-  speakTextWithAWSPolly,
   //speakTextWithAWSPolly,
   AgentDesktopPlugin
 };
