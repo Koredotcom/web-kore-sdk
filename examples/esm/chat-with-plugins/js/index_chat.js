ed94eeeb64fd12731f3a9d8d4a7f69692235a209
Santhosh Vishal <santhoshvishal.veerannapet@kore.com>
2022-10-27 18:51:29 +0530

    STAT-4654 desktop notifications when agent comes online

--- examples/esm/chat-with-plugins/js/index_chat.js
+++ examples/esm/chat-with-plugins/js/index_chat.js
@@ -1,12 +1,10 @@
-
-
-import {chatConfig,chatWindow} from '../../../../../dist/kore-web-sdk.esm.browser.js';
-import {KoreFileUploaderPlugin} from '../../../../../dist/kore-web-sdk.esm.browser.js';
-import {KorePickersPlugin } from '../../../../../dist/kore-web-sdk.esm.browser.js';
-import {GraphTemplatesPlugin} from '../../../../../dist/kore-web-sdk.esm.browser.js';
-import {WebKitSTT} from '../../../../../dist/kore-web-sdk.esm.browser.js';
-import {BrowserTTS} from '../../../../../dist/kore-web-sdk.esm.browser.js';
-import {AgentDesktopPlugin} from '../../../../../dist/kore-web-sdk.esm.browser.js';
+import { chatConfig, chatWindow } from '../../../../../dist/kore-web-sdk.esm.browser.js';
+import { KoreFileUploaderPlugin } from '../../../../../dist/kore-web-sdk.esm.browser.js';
+import { KorePickersPlugin } from '../../../../../dist/kore-web-sdk.esm.browser.js';
+import { GraphTemplatesPlugin } from '../../../../../dist/kore-web-sdk.esm.browser.js';
+import { WebKitSTT } from '../../../../../dist/kore-web-sdk.esm.browser.js';
+import { BrowserTTS } from '../../../../../dist/kore-web-sdk.esm.browser.js';
+import { AgentDesktopPlugin } from '../../../../../dist/kore-web-sdk.esm.browser.js';
 
 
 let chatWindowInstance = new chatWindow();
@@ -23,15 +21,15 @@ chatWindowInstance.installPlugin(new AgentDesktopPlugin());
 
 
 //OPTION #1
-let botOptions=chatConfig.botOptions;
+let botOptions = chatConfig.botOptions;
 
-botOptions.JWTUrl = "PLEASE_ENTER_JWTURL_HERE";
-botOptions.userIdentity = 'PLEASE_ENTER_USER_EMAIL_ID';// Provide users email id here
-botOptions.botInfo = { name: "PLEASE_ENTER_BOT_NAME", "_id": "PLEASE_ENTER_BOT_ID" }; // bot name is case sensitive
-botOptions.clientId = "PLEASE_ENTER_CLIENT_ID";
-botOptions.clientSecret = "PLEASE_ENTER_CLIENT_SECRET";
+botOptions.JWTUrl = "https://mk2r2rmj21.execute-api.us-east-1.amazonaws.com/dev/users/sts";
+botOptions.userIdentity = 'santhoshvishal.veerannapet@kore.com';// Provide users email id here
+botOptions.botInfo = { name: "Instance03", "_id": "st-b42e4f0d-3807-5866-90a3-57ed98505fe9" }; // bot name is case sensitive
+botOptions.clientId = "cs-394f098f-3914-5822-9d34-4bbafe081593";
+botOptions.clientSecret = "jBUKsjR/kVqmb5ovSMAvprT1ED7p8zob5Ya/4G4WlOY=";
 
-/* 
+/*
 Important Note: These keys are provided here for quick demos to generate JWT token at client side but not for Production environment.
 Refer below document for JWT token generation at server side. Client Id and Client secret should maintained at server end.
 https://developer.kore.ai/docs/bots/sdks/user-authorization-and-assertion/
@@ -41,35 +39,34 @@ https://developer.kore.ai/docs/bots/sdks/user-authorization-and-assertion/
 // //OPTION #2 with own JWT Service
 // var botOptions=chatConfig.botOptions;
 // botOptions.JWTUrl = "PLEASE_ENTER_JWTURL_HERE";
-// botOptions.botInfo = { 
-//     name: "PLEASE_ENTER_BOT_NAME",
-//     _id: "PLEASE_ENTER_BOT_ID" 
+// botOptions.botInfo = {
+// name: "PLEASE_ENTER_BOT_NAME",
+// _id: "PLEASE_ENTER_BOT_ID"
 // };
 // chatConfig.botOptions.userIdentity = 'PLEASE_ENTER_USER_EMAIL_ID';// Provide users email id here
 // chatConfig.JWTAsertion=function(commitJWT){
-//     chatWindowInstance.getJWT(chatConfig.botOptions).then(function(res){
-//         chatWindowInstance.setJWT(res.jwt);
-//         commitJWT();
-//     },function(errRes){
-//         console.log(errRes);
-//     });
-//  };
-//  chatWindowInstance.show(chatConfig);
- 
+// chatWindowInstance.getJWT(chatConfig.botOptions).then(function(res){
+// chatWindowInstance.setJWT(res.jwt);
+// commitJWT();
+// },function(errRes){
+// console.log(errRes);
+// });
+// };
+// chatWindowInstance.show(chatConfig);
 
 
-//  class customTemplateComponent{
-//     renderMessage(msgData){
-//         if(msgData?.message[0]?.component?.payload?.template_type==='custom_weather_template'){
-//             return '<h2>My Template HTML</h2>';      
-//         }else{
-//             return false;
-//         }
-//     } 
-//   }
-  
-//   chatWindowInstance.templateManager.installTemplate(new customTemplateComponent())
- 
- 
+
+// class customTemplateComponent{
+// renderMessage(msgData){
+// if(msgData?.message[0]?.component?.payload?.template_type==='custom_weather_template'){
+// return '<h2>My Template HTML</h2>';
+// }else{
+// return false;
+// }
+// }
+// }
+
+// chatWindowInstance.templateManager.installTemplate(new customTemplateComponent())
+
+
 chatWindowInstance.show(chatConfig);
-
--- src/plugins/agentDesktop/agentdesktop.ts
+++ src/plugins/agentDesktop/agentdesktop.ts
@@ -1,10 +1,15 @@
 import AgentDesktopPluginScript from './agentdesktop-script';
+import * as j$ from '../../libs/korejquery';
+import { event } from 'jquery';
 
+const $: any = j$.default;
 
 class AgentDesktopPlugin {
     name: any = 'AgentDesktopPlugin';
     config: any = {};
-    agentDesktopInfo :any;
+    agentDesktopInfo: any;
+    typingCount = 0;
+    doneTypingInterval = 500;
     constructor(config?: any) {
         this.config = {
             ...this.config,
@@ -13,13 +18,13 @@ class AgentDesktopPlugin {
 
     }
     onHostCreate() {
-        let me:any = this;
+        let me: any = this;
         let cwInstance = me.hostInstance;
 
-        cwInstance.on("jwtGrantSuccess", (response:any) => {
+        cwInstance.on("jwtGrantSuccess", (response: any) => {
             if (!this.agentDesktopInfo) {
                 me.onInit();
-                this.config=me.extend(me,response)
+                this.config = me.extend(me, response)
                 //me.AgentDesktop(response.userInfo.userId, response);
                 this.agentDesktopInfo = new AgentDesktopPluginScript(this.config);
             }
@@ -27,24 +32,136 @@ class AgentDesktopPlugin {
 
     }
     onInit() {
-        // let me = this;
+        console.log(Notification.permission, "message")
+        if (Notification.permission === "default") {
+            Notification.requestPermission();
+        }
+        let me: any = this;
         this.appendVideoAudioElemnts()
-        // this.koreCoBrowse = me.koreCoBrowseInit({});
-        // this.rrweb = me.rrwebInit({});
+
+        // send type event from user to agent
+        me.hostInstance.on('onKeyDown', ({ event }: any) => {
+            if (event.keyCode !== 13 && event.which <= 90 && event.which >= 48 || event.which >= 96 && event.which <= 105) {
+                if (this.typingCount == 0) {
+                    var messageToBot: any = {};
+                    // messageToBot["clientMessageId"] = new Date().getTime();
+                    messageToBot["event"] = "typing";
+                    messageToBot["message"] = {
+                        "body": "",
+                        "type": ""
+                    }
+                    messageToBot["resourceid"] = "/bot.message";
+                    // me.hostInstance.sendMessageToBot('', '', messageToBot)
+                    me.hostInstance.bot.sendMessage(messageToBot, (err: any) => {
+                        if (err && err.message) {
+                            console.log("Failed to send reciept", err, event.msgData)
+                        }
+                    });
+                    this.typingCount = 1;
+                }
+                clearTimeout(me.typingTimer);
+                me.typingTimer = setTimeout(me.doneTyping(), this.doneTypingInterval);
+
+            }
+        });
+
+        // typing event style setting for anget and bot typing
+        me.hostInstance.on('onWSMessage', (event: any) => {
+            console.log("websocketEvents", event)
+            if (event.messageData.type === "bot_response" && event.messageData?.message[0]?.component?.payload?.payload?.text) {
+                if (Notification.permission !== "granted") return;
+                // this.closeDesktopNotification();
+                let _self: any = this;
+                // console.log(event.messageData?.message)
+                console.log(event.messageData?.message[0]?.component?.payload?.payload?.text)
+                const text = event.messageData?.message[0]?.component?.payload?.payload?.text;
+                const image = "";
+                const title = "Agent is available now";
+                const options = {
+                    body: text,
+                    icon: "",
+                    vibrate: [200, 100, 200],
+                    sound: "https://uat.kore.ai/agents/sounds/Basic%20Alert%20High.mp3",
+                    badge: "",
+                    image: '',
+                    requireInteraction: false,
+                    onclick: (event: any) => {
+                        event.target.close();
+                    }
+                }
+                let notification: any
+                if (document.hidden) {
+                    notification = new Notification("You have a new notification ", options);
+                }
+                if (notification) {
+                    notification.onclick = function (event: any) {
+                        window.parent.focus();
+                        notification.close();
+                    };
+                }
+            }
+            if (event.messageData?.message?.author?.type === 'AGENT' && event.messageData.message.type === 'typing') {
+                $('.typingIndicatorContent').css('display', 'block');
+                $('.typingIndicator').text('agent');
+            } else if (event.messageData?.message?.author?.type === 'AGENT' && event.messageData.message.type === 'stoptyping') {
+                $('.typingIndicatorContent').css('display', 'none');
+            } else {
+                $('.typingIndicatorContent').css('display', 'block');
+                $('.typingIndicator').text('Bot');
+            }
+        });
+        // sent event style setting in user chat
+        me.hostInstance.on('afterRenderMessage', (event: any) => {
+            if (event.msgData?.type === "currentUser") {
+                const msg = event.msgData.message;
+                if (!event.messageHtml.children('.sentIndicator').length) {
+                    event.messageHtml.append('<div class="sentIndicator" style="visibility: visible;">sent</div>');
+                }
+            } else {
+                // send read event from user to agent
+                if (event.msgData?.message[0]?.component?.payload?.template_type == 'live_agent') {
+                    const messageToBot: any = {};
+                    messageToBot["event"] = "message_read";
+                    messageToBot["message"] = {
+                        "body": "",
+                        "type": ""
+                    }
+                    messageToBot["resourceid"] = "/bot.message";
+                    me.hostInstance.bot.sendMessage(messageToBot, (err: any) => {
+                        if (err && err.message) {
+                            console.log("Failed to send reciept", err, event.msgData)
+                        }
+                    });
+                }
+            }
+        });
+
+        // read event style setting in user chat
+        me.hostInstance.bot.on('message', (message: any) => {
+            // console.log("message", message)
+            var tempData = JSON.parse(message.data);
+            console.log("tempData >>>>>>>>>>", tempData);
+
+            if (!tempData) return;
+            if (tempData.from === "bot" && tempData.type === "events" && tempData.message.type === "message_read") {
+                $(".sentIndicator").text("Read");
+            }
+        });
+
     }
 
-    appendVideoAudioElemnts(){
-        let me:any = this;
+    appendVideoAudioElemnts() {
+        let me: any = this;
         let cwInstance = me.hostInstance;
         let chatEle = cwInstance.chatEle;
-        let localVideoElement ='<video id="kore_local_video" autoplay="autoplay" playsinline style="width:0px;height:0px"></video>';
-        let remoteVideoElement ='<video id="kore_remote_video" autoplay="autoplay" playsinline style="width:0px;height:0px"></video>';
+        let localVideoElement = '<video id="kore_local_video" autoplay="autoplay" playsinline style="width:0px;height:0px"></video>';
+        let remoteVideoElement = '<video id="kore_remote_video" autoplay="autoplay" playsinline style="width:0px;height:0px"></video>';
         chatEle.append(localVideoElement);
         chatEle.append(remoteVideoElement);
     }
 
-    extend(target:any, source:any) {
-        let me:any=this;
+    extend(target: any, source: any) {
+        let me: any = this;
         for (var prop in source) {
             if (source.hasOwnProperty(prop)) {
                 if (target[prop] && typeof source[prop] === 'object') {
@@ -56,6 +173,25 @@ class AgentDesktopPlugin {
             }
         }
         return target;
-      }
+    }
+
+    doneTyping() {
+        const me: any = this;
+        console.log("User stopped typing");
+        var messageToBot: any = {};
+        messageToBot["event"] = "stop_typing";
+        messageToBot["message"] = {
+            "body": "",
+            "type": ""
+        }
+        messageToBot["resourceid"] = "/bot.message";
+        // me.hostInstance.sendMessageToBot('', '', messageToBot)
+        me.hostInstance.bot.sendMessage(messageToBot, (err: any) => {
+            if (err && err.message) {
+                console.log("Failed to send reciept", err)
+            }
+        });
+        this.typingCount = 0;
+    }
 }
 export default AgentDesktopPlugin;
