170ad4c6b63babdf8918cac965b8da01ad2e53d4
Kambagiriswamy.Boya <kambagiriswamy.boya@KI0429-MacBook-Pro.local>
2022-11-10 23:01:41 +0530

    Read Events in AgentDesktop PlugIn

--- src/plugins/agentDesktop/agentdesktop.ts
+++ src/plugins/agentDesktop/agentdesktop.ts
@@ -1,10 +1,12 @@
 import AgentDesktopPluginScript from './agentdesktop-script';
+import * as j$ from '../../libs/korejquery';
 
+const $: any = j$.default;
 
 class AgentDesktopPlugin {
     name: any = 'AgentDesktopPlugin';
     config: any = {};
-    agentDesktopInfo :any;
+    agentDesktopInfo: any;
     constructor(config?: any) {
         this.config = {
             ...this.config,
@@ -13,13 +15,13 @@ class AgentDesktopPlugin {
 
     }
     onHostCreate() {
-        let me:any = this;
+        let me: any = this;
         let cwInstance = me.hostInstance;
 
-        cwInstance.on("jwtGrantSuccess", (response:any) => {
+        cwInstance.on("jwtGrantSuccess", (response: any) => {
             if (!this.agentDesktopInfo) {
                 me.onInit();
-                this.config=me.extend(me,response)
+                this.config = me.extend(me, response)
                 //me.AgentDesktop(response.userInfo.userId, response);
                 this.agentDesktopInfo = new AgentDesktopPluginScript(this.config);
             }
@@ -27,24 +29,81 @@ class AgentDesktopPlugin {
 
     }
     onInit() {
-        // let me = this;
+        let me: any = this;
         this.appendVideoAudioElemnts()
-        // this.koreCoBrowse = me.koreCoBrowseInit({});
-        // this.rrweb = me.rrwebInit({});
+
+        // agent connected and disconnected events
+        me.hostInstance.on('onWSMessage', (event: any) => {
+            if (event.messageData?.message?.type === 'agent_connected') {
+                localStorage.setItem("isAgentConnected", "true")
+            } else if (event.messageData?.message?.type === 'agent_disconnected') {
+                localStorage.setItem("isAgentConnected", "false")
+            }
+        });
+
+        // sent event style setting in user chat 
+        me.hostInstance.on('afterRenderMessage', (event: any) => {
+
+            if (localStorage.getItem("isAgentConnected") != "true") return;
+
+            if (event.msgData?.type === "currentUser") {
+                const msg = event.msgData.message;
+                if (!event.messageHtml.children('.sentIndicator').length) {
+                    event.messageHtml.append('<div class="sentIndicator">Sent</div>');
+
+                    me.hostInstance.bot.on('message', (message: any) => {
+                        var tempData = JSON.parse(message.data);
+                        if (!tempData) return;
+                        if (tempData.from === "bot" && tempData.type === "events" && tempData.message.clientMessageId === msg[0].clientMessageId) {
+
+                            if (tempData.message.type === "message_delivered") {
+                                if ($("#" + tempData.message.clientMessageId + " .sentIndicator").text() !== 'Read') {
+                                    $("#" + tempData.message.clientMessageId + " .sentIndicator").text("Delivered");
+                                }
+                            } else if (tempData.message.type === "message_read") {
+                                $("#" + tempData.message.clientMessageId + " .sentIndicator").text("Read");
+                            }
+
+                        }
+                    });
+                }
+            } else {
+                // send read event from user to agent 
+                if (event.msgData?.message[0]?.component?.payload?.template_type == 'live_agent') {
+                    const messageToBot: any = {};
+                    
+                    const msgId = event.msgData.messageId;
+                    messageToBot["event"] = "message_delivered";
+                    messageToBot["message"] = {
+                        "body": "",
+                        "type": ""
+                    }
+                    messageToBot["resourceid"] = "/bot.message";
+                    me.hostInstance.bot.sendMessage(messageToBot, (err: any) => {});
+
+                    if ($('#' + msgId).is(':visible')) {
+                        messageToBot.event = 'message_read'
+                        me.hostInstance.bot.sendMessage(messageToBot, (err: any) => {});
+                    }
+
+                }
+            }
+        });
+
     }
 
-    appendVideoAudioElemnts(){
-        let me:any = this;
+    appendVideoAudioElemnts() {
+        let me: any = this;
         let cwInstance = me.hostInstance;
         let chatEle = cwInstance.chatEle;
-        let localVideoElement ='<video id="kore_local_video" autoplay="autoplay" playsinline style="width:0px;height:0px"></video>';
-        let remoteVideoElement ='<video id="kore_remote_video" autoplay="autoplay" playsinline style="width:0px;height:0px"></video>';
+        let localVideoElement = '<video id="kore_local_video" autoplay="autoplay" playsinline style="width:0px;height:0px"></video>';
+        let remoteVideoElement = '<video id="kore_remote_video" autoplay="autoplay" playsinline style="width:0px;height:0px"></video>';
         chatEle.append(localVideoElement);
         chatEle.append(remoteVideoElement);
     }
 
-    extend(target:any, source:any) {
-        let me:any=this;
+    extend(target: any, source: any) {
+        let me: any = this;
         for (var prop in source) {
             if (source.hasOwnProperty(prop)) {
                 if (target[prop] && typeof source[prop] === 'object') {
@@ -56,6 +115,6 @@ class AgentDesktopPlugin {
             }
         }
         return target;
-      }
+    }
 }
 export default AgentDesktopPlugin;
