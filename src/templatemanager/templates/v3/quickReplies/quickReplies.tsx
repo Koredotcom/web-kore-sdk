aad7490308816af902c3f12f8b0a7dca2ab6cfbb
Venkateswara Rao <venkateswara.velidi@kore.com>
2023-07-27 16:01:02 +0530

    quick replies updated

--- src/templatemanager/templates/v3/quickReplies/quickReplies.tsx
+++ src/templatemanager/templates/v3/quickReplies/quickReplies.tsx
@@ -20,13 +20,95 @@ export function QuickReply(props: any) {
         }
         hostInstance.chatEle.querySelector('.quick-replies').remove();
     }
-    return (
-        <div className="quick-replies button-temp button-variation-2">
-            {msgData.message[0].component.payload.quick_replies.map((ele: any) => (
-                <button className="kr-btn" onClick={() => handleQuickReply(ele)}>{ele.title}</button>
-            ))
+    setTimeout(() => {
+        const btnsParentDiv: any = hostInstance.chatEle.querySelector('.quick-replies-buttons');
+        const leftScrollBtn = hostInstance.chatEle.querySelector('.quick-left-click');
+        const rightScrollBtn = hostInstance.chatEle.querySelector('.quick-right-click');
+        if (btnsParentDiv && btnsParentDiv.hasChildNodes()) {
+            if (leftScrollBtn) {
+                if (btnsParentDiv.scrollLeft > 0) {
+                    leftScrollBtn.classList.remove('hide');
+                } else {
+                    leftScrollBtn.classList.add('hide');
+                }
             }
+            if (rightScrollBtn) {
+                if (btnsParentDiv.offsetWidth < btnsParentDiv.scrollWidth) {
+                    rightScrollBtn.classList.remove('hide');
+                } else {
+                    rightScrollBtn.classList.add('hide');
+                }
+            }
+        }
+
+        leftScrollBtn.addEventListener('click', () => {
+            const btnsParentDivWidth = btnsParentDiv.scrollLeft;
+            const qButtons = btnsParentDiv.querySelectorAll('.quick-buttons');
+            let curWidth = 0;
+            if (qButtons.length > 0) {
+                qButtons.forEach((ele: any) => {
+                    curWidth = curWidth + ele.offsetWidth + 10;
+                    if (curWidth > btnsParentDivWidth) {
+                        btnsParentDiv.scrollTo({
+                            left: btnsParentDiv.offsetHeight - ele.offsetHeight - 50,
+                            behavior: 'smooth'
+                        });
+                        rightScrollBtn.classList.remove('hide');;
+                        if (btnsParentDiv.scrollLeft <= 0) {
+                            leftScrollBtn.classList.add('hide');;
+                        }
+                    }
+
+                })
+            }
+        })
+        rightScrollBtn.addEventListener('click', () => {
+            const btnsParentDivWidth = btnsParentDiv.offsetWidth;
+            const qButtons = btnsParentDiv.querySelectorAll('.quick-buttons');
+            let curWidth = 0;
+            if (qButtons.length > 0) {
+                qButtons.forEach((ele: any) => {
+                    curWidth = curWidth + ele.offsetWidth + 10;
+                    if (curWidth > btnsParentDivWidth) {
+                        btnsParentDiv.scrollTo({
+                            left: btnsParentDiv.scrollLeft + ele.offsetWidth + 20,
+                            behavior: 'smooth'
+                        });
+                        leftScrollBtn.classList.remove('hide');;
+                        if (btnsParentDiv.scrollLeft + btnsParentDivWidth + 10 >= btnsParentDiv.scrollWidth) {
+                            rightScrollBtn.classList.add('hide');
+                        }
+                    }
+
+                })
+            }
+        })
+    }, 50);
+    return (
+
+        <div className="quick-replies button-temp button-variation-2">
+            <button className="quick-left-click">
+                <i className="sdkv3-cheveron-left"></i>
+            </button>
+            <div className="quick-replies-buttons">
+                {msgData.message[0].component.payload.quick_replies.map((ele: any) => (
+                    <button className="kr-btn quick-buttons" onClick={() => handleQuickReply(ele)}>{ele.title}</button>
+                ))
+                }
+                {msgData.message[0].component.payload.quick_replies.map((ele: any) => (
+                    <button className="kr-btn quick-buttons" onClick={() => handleQuickReply(ele)}>{ele.title}</button>
+                ))
+                }
+                {msgData.message[0].component.payload.quick_replies.map((ele: any) => (
+                    <button className="kr-btn quick-buttons" onClick={() => handleQuickReply(ele)}>{ele.title}</button>
+                ))
+                }
+            </div>
+            <button className="quick-right-click">
+                <i className="sdkv3-cheveron-right"></i>
+            </button>
         </div>
+
     )
 }
 
