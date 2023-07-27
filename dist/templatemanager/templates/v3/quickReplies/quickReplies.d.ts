e4c707c0b5a684f164e12d314535163344683c27
Venkateswara Rao <venkateswara.velidi@kore.com>
2023-07-27 12:54:07 +0530

    quick replies template updated

--- dist/templatemanager/templates/v3/quickReplies/quickReplies.d.ts
+++ dist/templatemanager/templates/v3/quickReplies/quickReplies.d.ts
@@ -1,6 +1,7 @@
 import BaseChatTemplate from '../baseChatTemplate';
 import './quickReplies.scss';
 import { h } from 'preact';
+export declare function QuickReply(props: any): h.JSX.Element;
 export declare function QuickReplies(props: any): h.JSX.Element | undefined;
 declare class QuickRepliesTemplate extends BaseChatTemplate {
     hostInstance: any;
--- src/templatemanager/templates/v3/quickReplies/quickReplies.tsx
+++ src/templatemanager/templates/v3/quickReplies/quickReplies.tsx
@@ -3,32 +3,52 @@ import './quickReplies.scss';
 import { h, Fragment } from 'preact';
 import { useState } from 'preact/hooks';
 import { Message } from '../message/message';
-export function QuickReplies(props: any) {
-    const hostInstance = props.hostInstance;
+import { getHTML } from '../../../base/domManager';
+
+export function QuickReply(props: any) {
     const msgData = props.msgData;
-    const handleButtonEvent = (e: any) => {
-        if (e.type.toLowerCase() == 'postback' || e.type.toLowerCase() == 'text') {
-            hostInstance.sendMessage(e.payload || e.value, { renderMsg: e.title });
-        } else if (e.type == 'url' || e.type == 'web_url') {
+    const hostInstance = props.hostInstance;
+    const handleQuickReply = (e: any) => {
+        if (e.content_type.toLowerCase() == 'postback' || e.content_type.toLowerCase() == 'text') {
+            hostInstance.sendMessage(JSON.stringify(e.payload) || e.value, { renderMsg: e.title });
+        } else if (e.content_type == 'url' || e.content_type == 'web_url') {
             let link = e.url;
             if (link.indexOf('http:') < 0 && link.indexOf('https:') < 0) {
                 link = `http:////${link}`;
             }
             hostInstance.openExternalLink(link);
         }
+        hostInstance.chatEle.querySelector('.quick-replies').remove();
     }
+    return (
+        <div className="quick-replies button-temp button-variation-2">
+            {msgData.message[0].component.payload.quick_replies.map((ele: any) => (
+                <button className="kr-btn" onClick={() => handleQuickReply(ele)}>{ele.title}</button>
+            ))
+            }
+        </div>
+    )
+}
+
+export function QuickReplies(props: any) {
+    const hostInstance = props.hostInstance;
+    const msgData = props.msgData;
     const messageobj = {
         msgData: msgData,
         hostInstance: hostInstance
     }
-    if (msgData?.message?.[0]?.component?.payload?.template_type === 'quick_replies' && !msgData?.message?.[0]?.component?.formData) {
+    if (msgData?.message?.[0]?.component?.payload?.template_type === 'quick_replies') {
+        setTimeout(() => {
+            if (!msgData.fromHistory) {
+                const quickReply = getHTML(QuickReply, msgData, hostInstance);
+                const composeBar = hostInstance.chatEle.querySelector('.chat-widget-composebar');
+                composeBar.insertBefore(quickReply, composeBar.firstChild);
+            }
+        }, 500);
         return (
             <Fragment>
-                <div>
+                <div className='quick-replies-container'>
                     <Message {...messageobj} />
-                    <div className="quick-reply-container">
-                        <div>Hello Quick Reply</div>
-                    </div>
                 </div>
             </Fragment>
         );
