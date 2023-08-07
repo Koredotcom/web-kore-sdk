32c065c63d322f174bf7b0255a18de5d9d6ae062
Venkateswara Rao <venkateswara.velidi@kore.com>
2023-08-07 18:55:24 +0530

    dropdown and radio options templates updated

--- src/templatemanager/templates/v3/dropdown/dropdown.tsx
+++ src/templatemanager/templates/v3/dropdown/dropdown.tsx
@@ -7,44 +7,57 @@ import { Message } from '../message/message';
 export function Dropdown(props: any) {
     const hostInstance = props.hostInstance;
     const msgData = props.msgData;
+    const [selectedItem, setSelectedItem] = useState({ title: 'Select', value: 'Select' });
     const messageobj = {
         msgData: msgData,
         hostInstance: hostInstance
     }
+
+    const openDropdown = (e: any) => {
+        e.currentTarget.classList.toggle('show-drp');
+    }
+
+    const selectItem = (e: any, item: any) => {
+        setSelectedItem(item);
+    }
+
+    const onSubmit = () => {
+        hostInstance.sendMessage(selectedItem.value, { renderMsg: selectedItem.title });
+    }
+
     if (msgData?.message?.[0]?.component?.payload?.template_type == 'dropdown_template') {
+        if (msgData?.fromHistory) {
+            const selectedItem = msgData?.message?.[0]?.component?.payload?.elements.filter((e: any) => e.value == msgData?.message?.[0]?.component?.selectedValue);
+            setSelectedItem(selectedItem[0]);
+        }
         return (
             <section className="dropdwon-wrapper-section">
                 <div className="dropdwon-wrapper-contaner">
-                    <div className="heading-block">
-                        <h1>Fill the form</h1>
-                        <button className="close-dropdown">
+                    { msgData.message?.[0].component?.payload.heading && <div className="heading-block">
+                        <h1>{msgData.message?.[0].component?.payload.heading}</h1>
+                        {/* <button className="close-dropdown">
                             <i className="sdkv3-close"></i>
-                        </button>
-                    </div>
+                        </button> */}
+                    </div> }
                     <div className="dropdown-temp">
-                        <div className="label-text">Please select</div>
-                        <div className="drp-menu-wrapper">
+                        {/* <div className="label-text">Please select</div> */}
+                        <div className="drp-menu-wrapper" onClick={openDropdown}>
                             <button className="drp-btn">
-                                <span>Placeholder</span>
+                                <span>{selectedItem?.title}</span>
                                 <i className="sdkv3-back-arrow"></i>
                             </button>
                             <div className="menu-content-list-drp">
-                                <li className="active-list-option">
-                                    <p>Item 2</p>
-                                    <i className="sdkv3-check"></i>
-                                </li>
-                                <li>
-                                    <p>Item 2</p>
-                                    <i className="sdkv3-check"></i>
-                                </li>
-                                <li>
-                                    <p>Item 2</p>
-                                    <i className="sdkv3-check"></i>
-                                </li>
+                                { msgData?.message?.[0].component?.payload.elements.map((ele: any) => (
+                                    <li className={selectedItem?.title == ele?.title ? 'active-list-option' :''} onClick={event => selectItem(event, ele) }>
+                                        <p>{ele.title}</p>
+                                        {/* { selectedItem.title == ele.title && <i className="sdkv3-check"></i> } */}
+                                        <i className="sdkv3-check"></i>
+                                    </li>
+                                ))}           
                             </div>
                         </div>
                     </div>
-                    <button className="kr-button-primary lg">Submit</button>
+                    <button className="kr-button-primary lg" onClick={onSubmit}>Submit</button>
                 </div>
             </section>
         );
--- src/templatemanager/templates/v3/radioOptions/radioOptions.tsx
+++ src/templatemanager/templates/v3/radioOptions/radioOptions.tsx
@@ -11,14 +11,26 @@ export function RadioOptions(props: any) {
         msgData: msgData,
         hostInstance: hostInstance
     }
+
+    const selectedItem  = () => {
+
+    }
     if (msgData?.message?.[0]?.component?.payload?.template_type == 'radioOptionTemplate' && !msgData.fromHistory) {
         return (
-            <div className="radio-button-item">
-                <input id="radio-1" name="radio" className="radio-input" type="radio" />
-                <label for="radio-1" className="radio-label">
-                    <div className="radio-title">Radio button item</div>
-                    <div className="radio-desc">Radio button item</div>
-                </label>
+            <div className="radio-button-wrapper">
+                <div>{msgData?.message?.[0]?.component?.payload.heading}</div>
+                { msgData?.message?.[0]?.component?.payload.radioOptions.map((ele: any, ind: any) => (
+                    <div className="radio-padding">
+                        <div className="radio-button-item">
+                            <input id={`radio-${ind}`} name="radio" className="radio-input" type="radio" />
+                            <label for={`radio-${ind}`} className="radio-label">
+                                <div className="radio-title">{ele.title}</div>
+                                {/* <div className="radio-desc">Radio button item</div> */}
+                            </label>
+                        </div>
+                    </div>
+                ))}
+                <button className="kr-button-primary" onClick={selectedItem}>Confirm</button>
             </div>
         );
     }
