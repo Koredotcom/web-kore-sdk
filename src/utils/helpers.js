22911661c732bb721a00c99e0ba6bb0642999fbb
KI0546\Rakesh.Manda <rakesh.manda@kore.com>
2023-09-14 12:47:38 +0530

    table template changes and support in helpers to get text

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -522,6 +522,13 @@ class KoreHelpers{
             }
             val = txtArr.join('');
             return val;
+        },
+        'getInnerText': function(val){
+            if(val === ''){
+                return val;
+            }
+            var tempEle = $('<div>'+this.convertMDtoHTML(val,'bot')+'</div>')
+            return tempEle.text();
         }
     };
     static prototypes ={
