3bbf0372a8bef05e75926ea51671112764cf4d4e
VenkateswararaoVe-Kore <venkateswara.velidi@kore.com>
2023-12-13 23:09:48 +0530

    branding changes

--- src/templatemanager/base/chatContainer/chatContainer.tsx
+++ src/templatemanager/base/chatContainer/chatContainer.tsx
@@ -18,13 +18,15 @@ export function ChatContainer(props: any) {
         updateBrandingInfo({...event.brandingData})
     });
 
-    const themeType: any = {
-        light: 'chat-window-main-section minimize-chat light-theme',
-        dark: 'chat-window-main-section minimize-chat dark-theme'
+    let chatContainerClass = 'chat-window-main-section minimize-chat';
+    if (brandingInfo.chat_bubble.icon.size == 'medium') {
+        chatContainerClass = chatContainerClass + ' avatar-medium-size';
+    } else if (brandingInfo.chat_bubble.icon.size == 'large') {
+        chatContainerClass = chatContainerClass + ' avatar-large-size';
     }
 
     return (
-        <div className={themeType[brandingInfo.general.themeType]} aria-label='chat-window-section'>
+        <div className={chatContainerClass} aria-label='chat-window-section'>
             <div className="kr-wiz-menu-chat defaultTheme-kore"></div>
             <AvatarComponent {...props} />
             <ChatWidget {...props} />
