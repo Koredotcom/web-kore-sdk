4d02f7f0110dd50e10caf532cd1e6cc6d024cb0c
AbhishekSh-Kore <141325115+AbhishekSh-Kore@users.noreply.github.com>
2023-12-25 16:37:59 +0530

    feat: Temmplate UI

--- src/templatemanager/templates/v3/solutionTemplates/itemSelection/itemSelection.tsx
+++ src/templatemanager/templates/v3/solutionTemplates/itemSelection/itemSelection.tsx
@@ -17,14 +17,6 @@ export function RetailOrderSelection(props: any) {
     const [rerenderKey, setRerenderKey] = useState(0);
 
     useEffect(() => {
-        const wrappers = document.querySelectorAll('.card-template-wrapper');
-        const lastWrapper = wrappers[wrappers.length - 1] as HTMLElement;
-        if (lastWrapper && lastWrapper.classList.contains('card-template-wrapper')) {
-            lastWrapper.style.borderBottom = 'none';
-        }
-    }, []);
-
-    useEffect(() => {
         // Check if elements are being updated when the state changes
         // console.log('Updated elements:', elements);
     }, [elements]);
@@ -55,6 +47,7 @@ export function RetailOrderSelection(props: any) {
     // Function to handle increment
 
     const handleButtonEvent = (e: any) => {
+        console.log(e, 'e')
         if (e.type.toLowerCase() == 'postback' || e.type.toLowerCase() == 'text') {
             hostInstance.sendMessage(e.title || e.payload || e.value, { renderMsg: e.title });
         } else if (e.type == 'url' || e.type == 'web_url') {
@@ -79,19 +72,19 @@ export function RetailOrderSelection(props: any) {
     if (msgData?.message[0]?.component?.payload?.template_type === "retailOrderSelection" && msgData?.message[0]?.component?.payload?.card_type === 'detail') {
         console.log(msgData.message[0].component?.payload, 'msgData');
         return (
-            <div key={rerenderKey} className="list-action-template-wrapper">
-                <h2 className="m-title">{msgData.message[0].component?.payload?.title}</h2>
-                <div className="card-container">
-                    {
-                        msgData.message[0].component?.payload?.elements?.map((ele: any, index: number) => (
-                            <div>
+            <div>
+                <div key={rerenderKey} className="list-action-template-wrapper">
+                    <h2 className="m-title">{msgData.message[0].component?.payload?.title}</h2>
+                    <div className="card-container">
+                        {
+                            msgData.message[0].component?.payload?.elements?.map((ele: any, index: number) => (
+                                // <div>
                                 <div className={`card-template-wrapper ${ele.checkBox === "enabled" ? "check-box-style" : ""}`}>
                                     <div className="left-section">
                                         {
                                             ele.checkBox === "enabled" && (
                                                 <div class="kr-sg-checkbox">
                                                     <input id="checkbox-1" class="checkbox-custom" type="checkbox" />
-                                                    {/* <label for="checkbox-1" class="checkbox-custom-label">Checkbox item</label> */}
                                                 </div>
                                             )}
 
@@ -163,26 +156,145 @@ export function RetailOrderSelection(props: any) {
                                     }
                                 </div>
 
-                            </div>
-                        ))}
-                </div>
-                {
-                    msgData.message[0].component?.payload?.showMore === "true" && (
-                        <div className="show-more">
-                            <p className="m-title">{msgData.message[0].component?.payload?.showMoreTitle}</p>
-                        </div>
-                    )}
-                {
-                    msgData.message[0].component?.payload?.buttons &&
-                    <div className="btn-style">
-                        {
-                            msgData.message[0].component?.payload?.buttons?.map((button: any) => (
-                                // <div className="btn-style">
-                                <button style={button?.buttonStyle} className="shopping-btn" onClick={() => handleButtonEvent(button)}>{button?.title}</button>
-
+                                // </div>
                             ))}
                     </div>
-                }
+                    {
+                        msgData.message[0].component?.payload?.showMore === "true" && (
+                            <div className="show-more">
+                                <p className="m-title">{msgData.message[0].component?.payload?.showMoreTitle}</p>
+                            </div>
+                        )}
+                    {
+                        msgData.message[0].component?.payload?.buttons &&
+                        <div className="btn-style">
+                            {
+                                msgData.message[0].component?.payload?.buttons?.map((button: any) => (
+                                    // <div className="btn-style">
+                                    <button style={button?.buttonStyle} className="shopping-btn" onClick={() => handleButtonEvent(button)}>{button?.title}</button>
+
+                                ))}
+                        </div>
+                    }
+
+                </div>
+                {
+                    msgData.message[0].component?.payload?.elements?.map((ele: any, index: number) => (
+                        ele?.flag === "ItemdetailsScreen" && (
+                            <div className="list-action-template-wrapper">
+                                <h2 className="m-title">{msgData.message[0].component?.payload?.title}</h2>
+                                <div className="card-template-wrapper">
+                                    <div className="item-details">
+                                        <div className="left-section">
+                                            <div className="container-details m-gap">
+                                                <h1 className="title-style" style={ele?.titleStyle}>{ele?.title}</h1>
+
+                                            </div>
+                                            <div className="container-details m-gap">
+                                                <h2 className="sub-title-style" style={ele?.subTitleStyle}>{ele?.subTitle}</h2>
+                                            </div>
+                                            {
+                                                ele?.description && ele?.description?.map((ele: any, index: number) => (
+                                                    <div className="description">
+                                                        <div className="left-description">
+                                                            <p style={ele?.detailStyle}>{ele?.title}</p>
+                                                        </div>
+                                                        <div className="left-description">
+                                                            <p style={ele?.detailStyle}>{ele?.value}</p>
+                                                        </div>
+                                                    </div>
+                                                ))
+                                            }
+                                        </div>
+                                        <div className="right-section">
+                                            <img src={ele?.icon} />
+                                        </div>
+                                    </div>
+
+                                    {ele?.descriptionDetails &&
+                                        <div className="desc-details">
+                                            {
+                                                ele?.descriptionDetails?.map((ele: any, index: number) => (
+                                                    <div className=" description">
+                                                        {/* <div className="description"> */}
+                                                        <div className="left-description">
+                                                            <p style={ele?.titleStyle}>{ele?.title}</p>
+                                                        </div>
+                                                        <div className="right-description">
+                                                            <p style={ele?.valueStyle}>{ele?.value}</p>
+                                                        </div>
+                                                    </div>
+                                                    // </div>
+                                                ))
+                                            }
+                                        </div>
+                                    }
+                                    {
+                                        ele?.summaryDetails &&
+                                        <div className="summary-details">
+                                            {
+                                                ele?.summaryDetails?.map((ele: any, index: number) => (
+                                                    <div>
+                                                        <div className="desc-details-title">
+                                                            <h2>{ele?.title}</h2>
+                                                        </div>
+                                                        {
+                                                            ele?.description?.map((ele: any, index: number) => (
+                                                                <div className="description">
+                                                                    <div className="left-description">
+                                                                        <p>{ele?.title}</p>
+                                                                    </div>
+                                                                    <div className="right-description">
+                                                                        <p>{ele?.value}</p>
+                                                                    </div>
+                                                                </div>
+                                                            ))
+                                                        }
+                                                    </div>
+                                                ))
+                                            }
+                                        </div>
+                                    }
+                                    {
+                                        ele?.totalSummary &&
+                                        <div className="total-summary-details">
+                                            {
+                                                ele?.totalSummary?.map((ele: any, index: number) => (
+                                                    <div>
+                                                        <div className="desc-details-title">
+                                                            <div className="description">
+                                                                <div className="left-description">
+                                                                    <h2>{ele?.title}</h2>
+                                                                </div>
+                                                                <div className="right-description">
+                                                                    <h2>{ele?.value}</h2>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        {/* <div className="desc-details-title">
+                                                           
+                                                        </div> */}
+                                                        {/* {
+                                                            ele?.description?.map((ele: any, index: number) => (
+                                                                <div className="description">
+                                                                    <div className="left-description">
+                                                                        <p>{ele?.title}</p>
+                                                                    </div>
+                                                                    <div className="right-description">
+                                                                        <p>{ele?.value}</p>
+                                                                    </div>
+                                                                </div>
+                                                            ))
+                                                        } */}
+                                                    </div>
+                                                ))
+                                            }
+                                        </div>
+                                    }
+                                </div>
+                            </div>
+                        )
+                    ))}
 
             </div>
         );
