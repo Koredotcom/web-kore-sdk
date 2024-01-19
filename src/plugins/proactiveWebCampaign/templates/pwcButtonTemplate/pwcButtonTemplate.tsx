35ea83d3b467c1f8ad320c8d328d6a6e5d8aed70
devendarba-kore <devendar.bala@kore.com>
2024-01-19 20:06:10 +0530

    STAT-10827 styles updated

--- src/plugins/proactiveWebCampaign/templates/pwcButtonTemplate/pwcButtonTemplate.tsx
+++ src/plugins/proactiveWebCampaign/templates/pwcButtonTemplate/pwcButtonTemplate.tsx
@@ -3,9 +3,26 @@ import BaseChatTemplate from '../../../../templatemanager/templates/v3/baseChatT
 import './pwcButtonTemplate.scss';
 export function Button(props: any) {
     const msgData = props.msgData;
-    if (msgData.type == 'onTaskUpdate') {
+    // if (msgData.type == 'onTaskUpdate') {
+    if (msgData.type == 'Session_Start') {
         return (
-            <div>Button Template</div>
+            <div className="campaign-post-button-wrapper">
+                <div className="campaign-button-content-details">
+                    <button className="button-maintain">Maintenance</button>
+                    <div className="content-campaign-more-info">
+                        <div className="heading-block-info">
+                            <h1>Maintenance</h1>
+                            <button>
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                    <path d="M18 6L6 18M6 6L18 18" stroke="#667085" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                </svg>
+                            </button>
+                        </div>
+                        <p>The next maintenance of the website is been scheduled between at 02:00 am & 04:00 am</p>
+                        <a href="#" target="_blank">Know More</a>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
