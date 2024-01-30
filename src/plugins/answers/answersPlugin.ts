6ddfe3e766d58bde58e535ea9b593b217258550f
VenkateswararaoVe-Kore <venkateswara.velidi@kore.com>
2024-01-30 12:31:39 +0530

    answers plugin changes

--- src/plugins/answers/answersPlugin.ts
+++ src/plugins/answers/answersPlugin.ts
@@ -23,22 +23,12 @@ class AnswersTemplatesPlugin {
         cwInstance.on("viewInit", (chatWindowEle: any) => {
             me.onInit();
         });
-        cwInstance.on("onWSMessage", (chatWindowEle: any) => {
-            let messageData=chatWindowEle?.messageData;
-            if(messageData && messageData.message && messageData.message[0] && messageData.message[0].component && messageData.message[0].component.payload && messageData.message[0].component.payload.payload && messageData.message[0].component.payload.payload.template_type  === 'button') {
-                messageData.message[0].component.payload.payload.template_type = 'quick_replies_welcome';
-            }
-            
-        });
-
     }
 
     onInit() {
         let me = this;
         let $ = me.hostInstance.$;
         me.installPickerTemplates();
-        $('.kore-chat-window').addClass('solutions');
-
     }
     
     /**
