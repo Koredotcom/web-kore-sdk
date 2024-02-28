36c041def1b667c25edac1afb743d349b6aed9dc
VenkateswararaoVe-Kore <venkateswara.velidi@kore.com>
2024-02-28 10:05:22 +0530

    pwc avatar changes

--- src/templatemanager/base/avatarComponent/avatarComponent.tsx
+++ src/templatemanager/base/avatarComponent/avatarComponent.tsx
@@ -7,7 +7,7 @@ import { useEffect, useState } from 'preact/hooks';
 export function AvatarComponent(props: any) {
     const hostInstance = props.hostInstance;
     const [brandingInfo, updateBrandingInfo] = useState(hostInstance.config.branding);
-    const [pwcCampaign, updatePWCCampaignInfo] = useState({ enable: false, data: { buttons: [], messages: [], appearance: { messageBubbleAlignment : '', buttonAlignment: '', dropShadow: ''}}});
+    const [pwcCampaign, updatePWCCampaignInfo] = useState({ enable: false, data: { buttons: [], messages: [], appearance: { messageBubbleAlignment : '', buttonAlignment: '', dropShadow: ''}, messageHeaderConfig: { headerToggle: false, headerMessage: '', headerUpload: '', headerIcon: ''}}});
     hostInstance.on('onBrandingUpdate', function (event: any) {
         updateBrandingInfo({...event.brandingData})
     });
@@ -82,7 +82,7 @@ export function AvatarComponent(props: any) {
 
     const closePWCHelp = (e: any) => {
         hostInstance.chatEle.querySelector('.content-info').remove();
-        updatePWCCampaignInfo({ enable: false, data: { buttons: [], messages: [], appearance: { messageBubbleAlignment : '', buttonAlignment: '', dropShadow: pwcCampaign.data.appearance.dropShadow}}});
+        updatePWCCampaignInfo({ enable: false, data: { buttons: [], messages: [], appearance: { messageBubbleAlignment : '', buttonAlignment: '', dropShadow: pwcCampaign.data.appearance.dropShadow}, messageHeaderConfig: { headerToggle: false, headerMessage: '', headerUpload: '', headerIcon: ''}}});
     }
 
     const handlePWCButtonEvent = (e: any) => {
@@ -155,7 +155,13 @@ export function AvatarComponent(props: any) {
                 {pwcCampaign.enable && <div className="content-info">
                     {pwcCampaign.data?.messages.map((ele: any, ind: any) => (
                         <div className="text-content animation-slide-up" role="contentinfo" aria-labelledby="helojohn">
-                            <p className="help-text-content" dangerouslySetInnerHTML={{ __html: ele.value }}></p>
+                            <div className="help-text-content">
+                                {(ind == 0) && pwcCampaign.data.messageHeaderConfig.headerToggle && <div className="header-content">
+                                    {pwcCampaign.data.messageHeaderConfig.headerUpload == 'upload' && <img src={pwcCampaign.data.messageHeaderConfig.headerIcon}></img>}
+                                    {pwcCampaign.data.messageHeaderConfig.headerMessage && <h3>{pwcCampaign.data.messageHeaderConfig.headerMessage}</h3>}
+                                </div>}
+                                <p className="p-text-content" dangerouslySetInnerHTML={{ __html: ele.value }}></p>
+                            </div>
                             {(ind == 0) && <span className="close-avatar-content" role="contentinfo" aria-label="close" onClick={closePWCHelp}>
                                 <svg width="20" height="20" viewBox="0 0 20 20" fill="none">
                                     <path d="M10.8838 10.0001L16.0669 4.81694C16.311 4.57286 16.311 4.17714 16.0669 3.93306C15.8229 3.68898 15.4271 3.68898 15.1831 3.93306L9.99988 9.11624L4.81694 3.93352C4.57286 3.68944 4.17713 3.68945 3.93306 3.93354C3.68899 4.17762 3.689 4.57335 3.93308 4.81742L9.116 10.0001L3.93306 15.1831C3.68898 15.4272 3.68898 15.8229 3.93306 16.067C4.17714 16.311 4.57286 16.311 4.81694 16.067L9.9999 10.884L15.1831 16.067C15.4272 16.311 15.8229 16.311 16.067 16.0669C16.311 15.8229 16.311 15.4271 16.0669 15.1831L10.8838 10.0001Z" fill="#697586" />
