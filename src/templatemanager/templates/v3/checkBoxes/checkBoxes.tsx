ad1dfcd2d5db4655a56162f06eaeb321a7058905
VenkateswararaoVe-Kore <venkateswara.velidi@kore.com>
2024-03-07 10:15:04 +0530

    checkbox template issue fix

--- src/templatemanager/templates/v3/checkBoxes/checkBoxes.tsx
+++ src/templatemanager/templates/v3/checkBoxes/checkBoxes.tsx
@@ -7,9 +7,30 @@ import { Message } from '../message/message';
 export function CheckBoxes(props: any) {
     const hostInstance = props.hostInstance;
     const msgData = props.msgData;
-    const messageobj = {
-        msgData: msgData,
-        hostInstance: hostInstance
+
+    const onSelectAll = () => {
+        const selectedAll = hostInstance.chatEle.querySelector(`.checkbox-selectall-${msgData.messageId}`);
+        const eles = hostInstance.chatEle.querySelectorAll(`.checkbox-input-${msgData.messageId}`);
+        if (selectedAll && selectedAll.checked) {
+            eles.forEach((ele: any) => {
+                ele.checked = true;
+            });
+        } else {
+            eles.forEach((ele: any) => {
+                ele.checked = false;
+            });
+        }
+    }
+
+    const onItemSelect = () => {
+        const selectedAll = hostInstance.chatEle.querySelector(`.checkbox-selectall-${msgData.messageId}`);
+        const eles = hostInstance.chatEle.querySelectorAll(`.checkbox-input-${msgData.messageId}`);
+        let allChecked = true;
+        eles.forEach((ele: any) => {
+            if (!ele.checked) allChecked = false;
+        });
+        if (allChecked) selectedAll.checked = true
+        else selectedAll.checked = false
     }
 
     const onSubmit = () => {
@@ -24,18 +45,18 @@ export function CheckBoxes(props: any) {
     if (msgData?.message?.[0]?.component?.payload?.template_type == 'multi_select') {
         return (
             <div className="checkbox-wrapper">
-                <h1>Collections</h1>
-                <div className="checkbox-item select-all">
-                    <input id="checkbox-selectall" className="checkbox-input" type="checkbox" value="" />
-                    <label for="checkbox-selectall" className="checkbox-label">
+                {msgData.message[0].component.payload.heading && <h1>{msgData.message[0].component.payload.heading}</h1>}
+                <div className="checkbox-item select-all" onClick={() => onSelectAll()}>
+                    <input id={`checkbox-selectall-${msgData.messageId}`} className={`checkbox-input checkbox-selectall-${msgData.messageId}`} type="checkbox" value="" />
+                    <label for={`checkbox-selectall-${msgData.messageId}`} className="checkbox-label">
                         <div className="title">Select All</div>
                     </label>
                 </div>
                 <div className="checkbox-container">
                     { msgData?.message?.[0]?.component?.payload?.elements.map((ele: any, ind: any) => (
-                        <div className="checkbox-item">
-                            <input id={`checkbox-${ind}`} className={`checkbox-input checkbox-input-${msgData.messageId}`} type="checkbox" value={ele.value} />
-                            <label for={`checkbox-${ind}`} className="checkbox-label">
+                        <div className="checkbox-item" onClick={() => onItemSelect()}>
+                            <input id={`checkbox-${msgData.messageId}-${ind}`} className={`checkbox-input checkbox-input-${msgData.messageId}`} type="checkbox" value={ele.value} />
+                            <label for={`checkbox-${msgData.messageId}-${ind}`} className="checkbox-label">
                                 <div className="title">{ele.title}</div>
                                 {/* <div className="desc-text-checkbox">Checkbox item</div> */}
                             </label>
