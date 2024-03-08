4ef95b45ff29887a84378e938f3fb22060e172be
VenkateswararaoVe-Kore <venkateswara.velidi@kore.com>
2024-03-08 10:58:10 +0530

    dropdown fix

--- src/templatemanager/templates/v3/dropdown/dropdown.tsx
+++ src/templatemanager/templates/v3/dropdown/dropdown.tsx
@@ -7,7 +7,7 @@ import { Message } from '../message/message';
 export function Dropdown(props: any) {
     const hostInstance = props.hostInstance;
     const msgData = props.msgData;
-    const [selectedItem, setSelectedItem] = useState({ title: msgData.message?.[0].component?.payload?.placeholder, value: msgData.message?.[0].component?.payload?.placeholder });
+    const [selectedItem, setSelectedItem] = useState({ title: msgData.message?.[0].component?.payload?.placeholder ? msgData.message?.[0].component?.payload?.placeholder : 'Select', value: msgData.message?.[0].component?.payload?.placeholder ? msgData.message?.[0].component?.payload?.placeholder : 'Select' });
 
     const openDropdown = (e: any) => {
         e.currentTarget.classList.toggle('show-drp');
@@ -21,19 +21,26 @@ export function Dropdown(props: any) {
         hostInstance.sendMessage(selectedItem.value, { renderMsg: selectedItem.title });
     }
 
+    const removeDropdown = (msgId: any) => {
+        const ele = hostInstance.chatEle.querySelector(`.dropdown-${msgId}`);
+        if (ele) {
+            ele.remove();
+        }
+    }
+
     if (msgData?.message?.[0]?.component?.payload?.template_type == 'dropdown_template') {
         if (msgData?.fromHistory) {
             const selectedItem = msgData?.message?.[0]?.component?.payload?.elements.filter((e: any) => e.value == msgData?.message?.[0]?.component?.selectedValue);
             setSelectedItem(selectedItem[0]);
         }
         return (
-            <section className="dropdwon-wrapper-section">
+            <section className={`dropdwon-wrapper-section dropdown-${msgData.messageId}`}>
                 <div className="dropdwon-wrapper-contaner">
                     { msgData.message?.[0].component?.payload.heading && <div className="heading-block">
                         <h1>{msgData.message?.[0].component?.payload.heading}</h1>
-                        {/* <button className="close-dropdown">
+                        <button className="close-dropdown" onClick={() =>removeDropdown(msgData.messageId)}>
                             <i className="sdkv3-close"></i>
-                        </button> */}
+                        </button>
                     </div> }
                     <div className="dropdown-temp">
                         { msgData.message?.[0].component?.payload?.label && <div className="label-text">{msgData.message?.[0].component?.payload.label}</div> }
@@ -47,7 +54,10 @@ export function Dropdown(props: any) {
                             <div className="menu-content-list-drp">
                                 { msgData?.message?.[0].component?.payload.elements.map((ele: any) => (
                                     <li className={selectedItem?.title == ele?.title ? 'active-list-option' :''} onClick={event => selectItem(event, ele) }>
-                                        <p>{ele.title}</p>
+                                        <div className="list-section">
+                                            <p>{ele.title}</p>
+                                            <div className="subtext">{ele.value}</div>
+                                        </div>
                                         {/* { selectedItem.title == ele.title && <i className="sdkv3-check"></i> } */}
                                         <svg width="16" height="16" viewBox="0 0 16 16" fill="none">
                                             <path fill-rule="evenodd" clip-rule="evenodd" d="M13.7804 3.31768C13.967 3.11682 14.2795 3.1067 14.4784 3.29508C14.6656 3.47238 14.6855 3.76232 14.5317 3.96328L14.5008 3.99987L6.13818 12.509C5.95951 12.7013 5.66615 12.7183 5.46746 12.5556L5.43136 12.523L1.44799 8.55964C1.25373 8.36636 1.25144 8.05066 1.44287 7.85451C1.62304 7.66991 1.9106 7.65699 2.10576 7.81726L2.14122 7.84934L5.76405 11.454L13.7804 3.31768Z" fill="#202124"/>
