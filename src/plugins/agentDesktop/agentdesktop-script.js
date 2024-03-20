c20a12b55b2bf7e175c4cf2e700113470f440408
VijayMu-Kore <vijay.mulakidi@kore.com>
2024-03-20 15:58:30 +0530

    #PLAT-24444 audio call voice exchange delay issue fixed

--- src/plugins/agentDesktop/agentdesktop-script.js
+++ src/plugins/agentDesktop/agentdesktop-script.js
@@ -1116,7 +1116,11 @@ class AgentDesktopPluginScript  {
                 stream.getTracks().forEach(function (track) {
                     track.stop();
                 });
-                this.activeCall = this.phone.call(this.phone.VIDEO, sipUser);
+                if(this.callDetails.videoCall){
+                    this.activeCall = this.phone.call(this.phone.VIDEO, sipUser);
+                }else{
+                    this.activeCall = this.phone.call(this.phone.AUDIO, sipUser);
+                }
             });
         }
         this.toggleButtons = function () {
