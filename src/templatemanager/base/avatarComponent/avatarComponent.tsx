4a279784e8eac197bb0c287e3dccaf4496942f9e
devendarba-kore <devendar.bala@kore.com>
2024-03-20 13:50:27 +0530

    XOP-8391, XOP-8713 issue fixed

--- src/templatemanager/base/avatarComponent/avatarComponent.tsx
+++ src/templatemanager/base/avatarComponent/avatarComponent.tsx
@@ -209,11 +209,11 @@ export function AvatarComponent(props: any) {
                             <path d="M17.0492 13L12.2492 18M12.2492 18L7.44922 13M12.2492 18L12.2492 6" stroke="white" stroke-width="1.67" stroke-linecap="round" stroke-linejoin="round" />
                         </svg></span>}
                     </figure>}
-                    {brandingInfo.chat_bubble.icon.type == 'custom' && <figure>
-                        <img className="custom-img-uploaded" src={brandingInfo.chat_bubble.icon.icon_url} />
+                    {brandingInfo.chat_bubble.icon.type == 'custom' && <figure className="custom-img-uploaded">
+                        <img src={brandingInfo.chat_bubble.icon.icon_url} />
                     </figure>}
-                    {brandingInfo.chat_bubble.minimise.type == 'custom' && <figure>
-                        <img className="custom-img-uploaded-minimize" src={brandingInfo.chat_bubble.minimise.icon} />
+                    {brandingInfo.chat_bubble.minimise.type == 'custom' && <figure className="custom-img-uploaded-minimize" >
+                        <img src={brandingInfo.chat_bubble.minimise.icon} />
                     </figure>}
                 </button>
             </div>
