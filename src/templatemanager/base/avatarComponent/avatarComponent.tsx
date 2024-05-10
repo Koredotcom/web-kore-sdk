f8241a7f66862e8f3964df491f2f3d62b1054867
VenkateswararaoVe-Kore <venkateswara.velidi@kore.com>
2024-05-10 15:04:57 +0530

    avatar changes reverted

--- src/templatemanager/base/avatarComponent/avatarComponent.tsx
+++ src/templatemanager/base/avatarComponent/avatarComponent.tsx
@@ -106,7 +106,14 @@ export function AvatarComponent(props: any) {
         <div className={avatarParentStyle} aria-label="avatar footer">
             <div className={avatarStyle} aria-label="avatar actions">
                 {(hostInstance.config.branding.chat_bubble.proactive.show || hostInstance.config.branding.chat_bubble.proactive.header || hostInstance.config.branding.chat_bubble.proactive.messages.length > 0 || hostInstance.config.branding.chat_bubble.proactive.buttons.length > 0) && !hostInstance.config.pwcConfig.enable && <div className="content-info">
-                    <img src='/images/avatar.svg'/>
+                    {hostInstance.config.branding.chat_bubble.proactive.header && <div className="text-content animation-slide-up text-heading-one" role="contentinfo" aria-labelledby={dynamicContextResolver(hostInstance.config.branding.chat_bubble.proactive.header, hostInstance.config.UIContext)}>
+                        <h4 id="hellokore">{dynamicContextResolver(hostInstance.config.branding.chat_bubble.proactive.header, hostInstance.config.UIContext)}</h4>
+                        <span className="close-avatar-content" role="contentinfo" aria-label="close" onClick={closeHelp}>
+                            <svg width="14" height="14" viewBox="0 0 20 20" fill="none">
+                                <path d="M10.8838 10.0001L16.0669 4.81694C16.311 4.57286 16.311 4.17714 16.0669 3.93306C15.8229 3.68898 15.4271 3.68898 15.1831 3.93306L9.99988 9.11624L4.81694 3.93352C4.57286 3.68944 4.17713 3.68945 3.93306 3.93354C3.68899 4.17762 3.689 4.57335 3.93308 4.81742L9.116 10.0001L3.93306 15.1831C3.68898 15.4272 3.68898 15.8229 3.93306 16.067C4.17714 16.311 4.57286 16.311 4.81694 16.067L9.9999 10.884L15.1831 16.067C15.4272 16.311 15.8229 16.311 16.067 16.0669C16.311 15.8229 16.311 15.4271 16.0669 15.1831L10.8838 10.0001Z" fill="#697586" />
+                            </svg>
+                        </span>
+                    </div>}
                     {hostInstance.config.branding.chat_bubble.proactive.messages.map((msg: any, ind: any) => (
                         (msg.title && <div className="text-content animation-slide-up text-heading-one" role="contentinfo" aria-labelledby={msg.title}>
                             <p className="help-text-content">{dynamicContextResolver(msg.title, hostInstance.config.UIContext)}</p>
