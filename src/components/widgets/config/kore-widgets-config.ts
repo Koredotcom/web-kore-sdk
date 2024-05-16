75330330db37e082e3a72dc0520788bf4aebfb58
VenkateswararaoVe-Kore <venkateswara.velidi@kore.com>
2024-05-16 13:18:05 +0530

    widgets api url changed

--- src/components/widgets/config/kore-widgets-config.ts
+++ src/components/widgets/config/kore-widgets-config.ts
@@ -1,6 +1,6 @@
 var botOptionsWiz:any = {};
 botOptionsWiz.logLevel = 'debug';
-botOptionsWiz.koreAPIUrl = "https://bots.kore.ai";
+botOptionsWiz.koreAPIUrl = "https://platform.kore.ai";
 
 botOptionsWiz.JWTUrl = "PLEASE_ENTER_JWTURL_HERE";
 botOptionsWiz.userIdentity = 'PLEASE_ENTER_USER_EMAIL_ID';// Provide users email id here
