c9901beb61168f0670961aa5bc79ee39e6334b61
VenkateswararaoVe-Kore <131746603+VenkateswararaoVe-Kore@users.noreply.github.com>
2024-08-08 10:10:42 +0530

    checklist template duplicate issue fix

--- src/templatemanager/templates/checkListTemplate/checkList.ts
+++ src/templatemanager/templates/checkListTemplate/checkList.ts
@@ -110,7 +110,7 @@ class CheckListTemplate {
     getTemplateString() {
         var checkListTemplate = '<script id="chat_checklist_tmpl" type="text/x-jqury-tmpl"> \
         {{if msgData.message}} \
-            <li {{if msgData.type !== "bot_response"}} id="msg_${msgItem.clientMessageId}"{{/if}} class="{{if msgData.type === "bot_response"}}fromOtherUsers{{else}}fromCurrentUser{{/if}} with-icon"> \
+            <li class="{{if msgData.type === "bot_response"}}fromOtherUsers{{else}}fromCurrentUser{{/if}} with-icon" id="${msgData.messageId || msgItem.clientMessageId}"> \
                <div class="check-list-template">\
                        {{if msgData.createdOn}}<div class="extra-info">${helpers.formatDate(msgData.createdOn)}</div>{{/if}} \
                     {{if msgData.icon}}<div class="profile-photo"> <div class="user-account avtar" style="background-image:url(${msgData.icon})"></div> </div> {{/if}} \
