8d2de89e61268a9d1855a8c5003a7b39ca077f96
VenkateswararaoVe-Kore <131746603+VenkateswararaoVe-Kore@users.noreply.github.com>
2024-10-24 16:15:20 +0530

    updated code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,15 +18,34 @@ export class AppComponent implements AfterViewInit {
 
   ngAfterViewInit() {
     const chatWindowInstance = new chatWindow();
-    var botOptions = chatConfig.botOptions;
+    let botOptions = chatConfig.botOptions;
 
-    botOptions.koreAPIUrl = "https://platform.kore.ai/api/";
-    botOptions.JWTUrl = "https://mk2r2rmj21.execute-api.us-east-1.amazonaws.com/dev/users/sts";
-    botOptions.userIdentity = 'Venkat';// Provide users email id here
-    botOptions.botInfo = { name: "SDKDemo", "_id": "st-f59fda8f-e42c-5c6a-bc55-3395c109862a" }; // bot name is case sensitive
-    botOptions.clientId = "cs-8fa81912-0b49-544a-848e-1ce84e7d2df6";
-    botOptions.clientSecret = "DnY4BIXBR0Ytmvdb3yI3Lvfri/iDc/UOsxY2tChs7SY=";
-    chatWindowInstance.installPlugin(new WebKitSTT({ lang: 'en-US' }));
+    botOptions.JWTUrl = 'PLEASE_ENTER_JWTURL_HERE';
+    botOptions.userIdentity = 'PLEASE_ENTER_USER_EMAIL_ID'; // Provide users email id here
+    botOptions.botInfo = {
+      name: 'PLEASE_ENTER_BOT_NAME',
+      _id: 'PLEASE_ENTER_BOT_ID',
+    }; // bot name is case sensitive
+    botOptions.clientId = 'PLEASE_ENTER_CLIENT_ID';
+    botOptions.clientSecret = 'PLEASE_ENTER_CLIENT_SECRET';
+    /* 
+    Important Note: These keys are provided here for quick demos to generate JWT token at client side but not for Production environment.
+    Refer below document for JWT token generation at server side. Client Id and Client secret should maintained at server end.
+    https://developer.kore.ai/docs/bots/sdks/user-authorization-and-assertion/
+    **/
+    // Own JWT Service
+    // let botOptions = chatConfig.botOptions;
+    // botOptions.JWTUrl = "PLEASE_ENTER_JWTURL_HERE";
+    // botOptions.botInfo = {
+    //     name: "PLEASE_ENTER_BOT_NAME",
+    //     _id: "PLEASE_ENTER_BOT_ID"
+    // };
+    // chatConfig.botOptions.userIdentity = 'PLEASE_ENTER_USER_EMAIL_ID';// Provide users email id here
+    // chatConfig.JWTAsertion = function(commitJWT) {
+    //     // call API service to get jwt response
+    //     chatWindowInstance.setJWT(res.jwt);
+    //     commitJWT();
+    //  };
 
     // Install the custom template
     chatWindowInstance.templateManager.installTemplate(new DOMManager(this.componentFactoryResolver, this.injector, this.appRef));
