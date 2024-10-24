0cf7bdbb9c5e31e7ec47f77b870c8d9ac931d8a7
VenkateswararaoVe-Kore <131746603+VenkateswararaoVe-Kore@users.noreply.github.com>
2024-10-24 11:51:23 +0530

    added JSON sample for custom template example

--- src/WeatherCard.js
+++ src/WeatherCard.js
@@ -3,13 +3,25 @@ function WeatherCard(msgData) {
         alert("hello");
     }
 
+    /*
+    JSON for the below HTML
+
+    {
+        "type": "template",
+        "payload": {
+            "temparature": "38 C",
+            "template_type": "custom_weather_template"
+        }
+    }
+    */
+
     return (
         <>
             {<div style={{ background: "#c1c172" }}>
                 <div>
                     <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" />
                     <h1>Temp {msgData.message[0].component.payload.temparature}</h1>
-                    <p>weather-template works!{msgData.messageId}</p>
+                    <p>weather-template works! msgId-{msgData.messageId}</p>
                     <button onClick={testMe}>Test</button>
                 </div>
             </div>}
