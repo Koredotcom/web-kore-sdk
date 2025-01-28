8c9fe7d66c9d925e50d38527550925c0e656f2bb
VenkateswararaoVe-Kore <131746603+VenkateswararaoVe-Kore@users.noreply.github.com>
2025-01-28 18:30:59 +0530

    chat open by default and full screen mode

--- src/App.js
+++ src/App.js
@@ -9,38 +9,37 @@ function App() {
   useEffect(() => {
     if (!initialized) {
       initialized = true
-  
-      let botOptions = chatConfig.botOptions;
 
-      botOptions.JWTUrl = 'PLEASE_ENTER_JWTURL_HERE';
-      botOptions.userIdentity = 'PLEASE_ENTER_USER_EMAIL_ID'; // Provide users email id here
-      botOptions.botInfo = {
-        name: 'PLEASE_ENTER_BOT_NAME',
-        _id: 'PLEASE_ENTER_BOT_ID',
-      }; // bot name is case sensitive
-      botOptions.clientId = 'PLEASE_ENTER_CLIENT_ID';
-      botOptions.clientSecret = 'PLEASE_ENTER_CLIENT_SECRET';
       /* 
       Important Note: These keys are provided here for quick demos to generate JWT token at client side but not for Production environment.
       Refer below document for JWT token generation at server side. Client Id and Client secret should maintained at server end.
       https://developer.kore.ai/docs/bots/sdks/user-authorization-and-assertion/
       **/
       // Own JWT Service
-      // let botOptions = chatConfig.botOptions;
-      // botOptions.JWTUrl = "PLEASE_ENTER_JWTURL_HERE";
-      // botOptions.botInfo = {
-      //     name: "PLEASE_ENTER_BOT_NAME",
-      //     _id: "PLEASE_ENTER_BOT_ID"
-      // };
-      // chatConfig.botOptions.userIdentity = 'PLEASE_ENTER_USER_EMAIL_ID';// Provide users email id here
-      // chatConfig.JWTAsertion = function(commitJWT) {
-      //     // call API service to get jwt response
-      //     chatWindowInstance.setJWT(res.jwt);
-      //     commitJWT();
-      //  };
+      let botOptions = chatConfig.botOptions;
+      botOptions.JWTUrl = "PLEASE_ENTER_JWTURL_HERE";
+      botOptions.botInfo = {
+          name: "PLEASE_ENTER_BOT_NAME",
+          _id: "PLEASE_ENTER_BOT_ID"
+      };
+      chatConfig.botOptions.userIdentity = 'PLEASE_ENTER_USER_EMAIL_ID';// Provide users email id here
+      chatConfig.JWTAsertion = function(commitJWT) {
+        // call API service to get jwt response
+        chatWindowInstance.setJWT(res.jwt);
+        commitJWT();
+      };
 
       let chatWindowInstance = new chatWindow(chatConfig);
       chatWindowInstance.show(chatConfig);
+
+      // To open chat directly without clicking avatar icon
+      let viewInit = chatWindowInstance.EVENTS.VIEW_INIT;
+      chatWindowInstance.on(viewInit, (event) => { 
+        setTimeout(() => {
+          chatWindowInstance.chatEle.querySelector('.avatar-bg').click();
+        }, 800);
+      });
+
 
       // Customization using Events
       // let beforeRenderMessage = chatWindowInstance.EVENTS.BEFORE_RENDER_MSG;
@@ -52,21 +51,8 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Kore Web SDK
     </div>
   );
 }
