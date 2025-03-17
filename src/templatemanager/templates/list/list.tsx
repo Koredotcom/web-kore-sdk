10d5c674942ebd6b44c7d39e977a46e7636208af
VenkateswararaoVe-Kore <131746603+VenkateswararaoVe-Kore@users.noreply.github.com>
2025-03-17 10:38:19 +0530

    image broken issue and header issue fix in list template

--- src/templatemanager/templates/list/list.tsx
+++ src/templatemanager/templates/list/list.tsx
@@ -1,16 +1,13 @@
 import BaseChatTemplate from '../baseChatTemplate';
 import './list.scss';
 import { h, Fragment } from 'preact';
-import { useState } from 'preact/hooks';
-import { Message } from '../message/message';
-import IconsManager from '../../base/iconsManager';
 import { getHTML } from '../../base/domManager';
 import KoreHelpers from '../../../utils/helpers';
 
 export function ListMore(props: any) {
-    const iconHelper = new IconsManager();
     const hostInstance = props.hostInstance;
     const msgData = props.msgData.msgData;
+    const helpers = KoreHelpers.helpers;
     const closeMenu = () => {
         hostInstance.chatEle.querySelector('.chat-actions-bottom-wraper').classList.add('close-bottom-slide');
         setTimeout(() => {
@@ -34,7 +31,7 @@ export function ListMore(props: any) {
     return (
         <div className="menu-wrapper-data-actions">
             <div className="actions-slider-header-menu">
-                {/* <h1>Best Collections</h1> */}
+                {(msgData.message[0].component.payload.text || msgData.message[0].component.payload.heading) && <h1 dangerouslySetInnerHTML={{ __html: helpers.convertMDtoHTML((msgData.message[0].component.payload.text || msgData.message[0].component.payload.heading), "bot") }}></h1>}
                 <button className="menu-close" role="contentinfo" aria-label="close" onClick={closeMenu}>
                     <svg width="16" height="16" viewBox="0 0 20 20" fill="none">
                         <path d="M10.8838 10.0001L16.0669 4.81694C16.311 4.57286 16.311 4.17714 16.0669 3.93306C15.8229 3.68898 15.4271 3.68898 15.1831 3.93306L9.99988 9.11624L4.81694 3.93352C4.57286 3.68944 4.17713 3.68945 3.93306 3.93354C3.68899 4.17762 3.689 4.57335 3.93308 4.81742L9.116 10.0001L3.93306 15.1831C3.68898 15.4272 3.68898 15.8229 3.93306 16.067C4.17714 16.311 4.57286 16.311 4.81694 16.067L9.9999 10.884L15.1831 16.067C15.4272 16.311 15.8229 16.311 16.067 16.0669C16.311 15.8229 16.311 15.4271 16.0669 15.1831L10.8838 10.0001Z" fill="#697586"/>
@@ -47,14 +44,14 @@ export function ListMore(props: any) {
                         {msgData.message[0].component.payload.elements.map((ele: any, ind: any) => (
                             <div className="list-data-temp">
                                 <div className="img-with-content-block">
-                                    <div className="img-block">
+                                    {ele.image_url && <div className="img-block">
                                         <figure>
                                             <img src={ele.image_url} />
                                         </figure>
-                                    </div>
+                                    </div>}
                                     <div className="content-details">
-                                        <h1>{ele.title}</h1>
-                                        <p>{ele.subtitle}</p>
+                                        <h1 dangerouslySetInnerHTML={{ __html: helpers.convertMDtoHTML(ele.title, "bot") }}></h1>
+                                        <p dangerouslySetInnerHTML={{ __html: helpers.convertMDtoHTML(ele.subtitle, "bot") }}></p>
                                     </div>
                                 </div>
                                 {ele.buttons && ele.buttons[0] && <button className="kr-button-blue-light" onClick={() => handleClick(ele.buttons[0])}>{ele.buttons[0].title}</button>}
@@ -99,17 +96,17 @@ export function List(props: any) {
     if (msgData?.message?.[0]?.component?.payload?.template_type == 'list') {
         return (
             <div className="list-action-template-wrapper" data-cw-msg-id={msgData?.messageId}>
-                {/* <h1>Best Collections</h1> */}
+                {(msgData.message[0].component.payload.text || msgData.message[0].component.payload.heading) && <h1 dangerouslySetInnerHTML={{ __html: helpers.convertMDtoHTML((msgData.message[0].component.payload.text || msgData.message[0].component.payload.heading), "bot") }}></h1>}
                 <div className="list-content-details">
                     {msgData.message[0].component.payload.elements.map((ele: any, ind: any) => (
                         ( ind < 3 &&
                         <div className="list-data-temp">
                             <div className="img-with-content-block">
-                                <div className="img-block">
+                                {ele.image_url && <div className="img-block">
                                     <figure>
                                         <img src={ele.image_url} />
                                     </figure>
-                                </div>
+                                </div>}
                                 <div className="content-details">
                                     <h1 dangerouslySetInnerHTML={{ __html: helpers.convertMDtoHTML(ele.title, "bot") }}></h1>
                                     <p dangerouslySetInnerHTML={{ __html: helpers.convertMDtoHTML(ele.subtitle, "bot") }}></p>
