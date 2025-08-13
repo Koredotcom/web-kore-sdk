998619d04fcbdff3c2515c03ad9ad2d868d7d883
VenkateswararaoVe-Kore <131746603+VenkateswararaoVe-Kore@users.noreply.github.com>
2025-08-13 13:29:26 +0530

    chat widget unused code removed.

--- src/templatemanager/base/chatWidget/chatWidget.tsx
+++ src/templatemanager/base/chatWidget/chatWidget.tsx
@@ -5,20 +5,13 @@ import { h } from 'preact';
 import { ChatWidgetHeader } from '../chatWidgetHeader/chatWidgetHeader';
 import { ChatWidgetBody } from '../chatWidgetBody/chatWidgetBody';
 import { ChatWidgetComposeBar } from '../chatWidgetComposeBar/chatWidgetComposeBar';
-// Click-to-call functionality has been moved to agentDesktop plugin
-// import { ClickToCallComponent } from '../clickToCall/clickToCallComponent';
-import { useState } from 'preact/hooks';
 export function ChatWidget(props: any) {
-
-    const [showClickToCallWidget, setShowClickToCallWidget] = useState(false);
 
     return (
         <div className='chat-widgetwrapper-main-container'>
             <ChatWidgetHeader {...props}/>
             <ChatWidgetBody />
-            <ChatWidgetComposeBar {...props} setShowClickToCallWidget={setShowClickToCallWidget} />
-        {/* Click-to-call widget now handled by agentDesktop plugin */}
-        {/* {showClickToCallWidget && <ClickToCallComponent {...props} setShowClickToCallWidget={setShowClickToCallWidget} />} */}
+            <ChatWidgetComposeBar {...props} />
         </div>
     );
 }
