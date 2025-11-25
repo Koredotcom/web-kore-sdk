91c9a0b31d30d79d4a9d6082780fc498e1dc43b7
VenkateswararaoVe-Kore <venkateswara.velidi@kore.com>
2025-11-25 14:35:00 +0530

    dropped global bot singleton and scoped instance to constructor and moved _chatHistoryLoaded to inside kore bot

--- src/components/base-sdk/kore-bot-sdk-client.js
+++ src/components/base-sdk/kore-bot-sdk-client.js
@@ -54,6 +54,7 @@ let requireKr=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeo
     this.historyOffset = 0;
     this.previousHistoryLoading = false;
     this.paginatedScrollDataAvailable = true;
+    this._chatHistoryLoaded = false;
   }
   var userLocation = {
       "city": "",
@@ -62,7 +63,7 @@ let requireKr=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeo
       "longitude": 0,
       "street": ""
   };
-  var _chatHistoryLoaded = false;
+
   inherits(KoreBot, EventEmitter);
   
   KoreBot.prototype.emit = function emit() {
@@ -71,7 +72,7 @@ let requireKr=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeo
       arguments[2]="historysync";
       this.cbBotChatHistory(arguments);
     }else if(!window._chatHistoryLoaded && arguments && arguments[0] === 'history') {
-      _chatHistoryLoaded = true;
+      this._chatHistoryLoaded = true;
       this.cbBotChatHistory(arguments);
     }
     this.EventEmitter.prototype.emit.apply(this, arguments);
@@ -362,7 +363,7 @@ let requireKr=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeo
     if (this.RtmClient) {
       this.RtmClient._close();
     }
-    _chatHistoryLoaded = false;
+    this._chatHistoryLoaded = false;
     this.removeAllListeners();
   };
   /*
@@ -547,7 +548,7 @@ let requireKr=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeo
   KoreBot.prototype.onOpenWSConnection = function(msg) {
     debug("opened WS Connection");
     this.initialized = true;
-    if(this.options.loadHistory && !_chatHistoryLoaded){
+    if(this.options.loadHistory && !this._chatHistoryLoaded){
       this.getHistory({ limit : this.options.messageHistoryLimit });
     }
     this.emit(RTM_CLIENT_EVENTS.RTM_CONNECTION_OPENED, {});
@@ -639,7 +640,7 @@ let requireKr=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeo
       if(this.options.webhookConfig && this.options.webhookConfig.enable){
         this.options.webhookConfig.token=this.options.assertion;
         this.emit(WEB_EVENTS.WEB_HOOK_READY);
-        if(this.options.loadHistory && !_chatHistoryLoaded){
+        if(this.options.loadHistory && !this._chatHistoryLoaded){
           this.getHistory({ limit : this.options.messageHistoryLimit },data);
         }else{
           this.emit(WEB_EVENTS.WEB_HOOK_RECONNECTED);
@@ -709,7 +710,6 @@ let requireKr=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeo
   }
   
   module.exports.instance = function(){
-    _chatHistoryLoaded = false;
       var _instance=new KoreBot();
       /*
         Adding KoreBot,KoreRTMClient function to instance 
