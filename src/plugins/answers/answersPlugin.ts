aa795229f2d4625c7c066e9b22faaac024798317
JagadeeshT-Kore <jagadeesh.thatikonda@kore.com>
2025-12-10 14:45:55 +0530

    fixed settings api failing and access token problem (#273)
    Approved by: Venkateswara Rao Velidi

--- src/plugins/answers/answersPlugin.ts
+++ src/plugins/answers/answersPlugin.ts
@@ -22,6 +22,8 @@ class AnswersTemplatesPlugin {
         let cwInstance = me.hostInstance;
         cwInstance.on("viewInit", (chatWindowEle: any) => {
             me.onInit();
+        });
+        cwInstance.on("jwtGrantSuccess", (response: any) => {
             me.getFeedbackSettings();
         });
     }
@@ -43,7 +45,7 @@ class AnswersTemplatesPlugin {
         let me: any = this;
         let $ = me.hostInstance.$;
         return $.ajax({
-            url: me.hostInstance.config?.botOptions?.koreAPIUrl + "searchsdk/" + me.hostInstance.config?.botOptions?.botInfo?.taskBotId + "/settings",
+            url: me.hostInstance.config?.botOptions?.koreAPIUrl?.replace(/\/?$/, "/") + "searchsdk/" + me.hostInstance.config?.botOptions?.botInfo?.taskBotId + "/settings",
             type: "GET",
             headers: {
                 "Authorization": "bearer " + me.hostInstance.config?.botOptions?.accessToken
