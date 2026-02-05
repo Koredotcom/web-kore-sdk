d84dbb4b5e5487f728a6cd7a40e6a54fe8e6d7c5
VenkateswararaoVe-Kore <venkateswara.velidi@kore.com>
2026-02-05 13:01:24 +0530

    new line

--- examples/esm/chat/js/index_chat.js
+++ examples/esm/chat/js/index_chat.js
@@ -12,6 +12,7 @@ botOptions.botInfo = { name: "PLEASE_ENTER_BOT_NAME", "_id": "PLEASE_ENTER_BOT_I
 botOptions.clientId = "PLEASE_ENTER_CLIENT_ID";
 botOptions.clientSecret = "PLEASE_ENTER_CLIENT_SECRET";
 
+
 /* 
 Important Note: These keys are provided here for quick demos to generate JWT token at client side but not for Production environment.
 Refer below document for JWT token generation at server side. Client Id and Client secret should maintained at server end.
