58c1c318de3704876085fd25c97b3364f74d1dcb
VenkateswararaoVe-Kore <131746603+VenkateswararaoVe-Kore@users.noreply.github.com>
2026-08-04 18:24:42 +0530

    added support to connect to platform without chat window UI (#327)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import chatWindow from './components/chatwindow/chatWindow';
 import chatConfig from './components/chatwindow/config/kore-config';
+import { createBotInstance } from './components/base-sdk/kore-bot-sdk-client';
 
 import Korei18nPlugin from './plugins/i18n';
 import KoreFileUploaderPlugin from './plugins/fileUploader/fileUploader';
@@ -70,5 +71,6 @@ export {
   ProactiveWebCampaignPlugin,
   V2Plugin,
   V2WidgetsConfig,
-  V2KoreWidgetSDK
+  V2KoreWidgetSDK,
+  createBotInstance
 };
